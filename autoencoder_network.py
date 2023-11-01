4f12fe8066a874711af9e03d9d0a57455b90724b
Sam Howie <samhowie9@gmail.com>
2023-11-01 12:37:43 +0000

    Adding partial plots

--- autoencoder_network.py
+++ autoencoder_network.py
@@ -73,32 +73,32 @@ test_images = np.expand_dims(test_images, axis=3)
 
 
 # create a figure to store the two different plots
-fig, (ax1, ax2) = plt.subplots(1,2, figsize=(18,6))
-
-# set the title, labels and accuracy axis limits of the training data plot
-ax1.set_title("Training Loss")
-ax1.set_ylabel("Loss")
-ax1.set_xlabel("Epochs")
-
-# set the title, labels and accuracy axis limits of the validation data plot
-ax2.set_title("Validation Loss")
-ax2.set_ylabel("Loss")
-ax2.set_xlabel("Epochs")
+# fig, (ax1, ax2) = plt.subplots(1,2, figsize=(18,6))
+#
+# # set the title, labels and accuracy axis limits of the training data plot
+# ax1.set_title("Training Loss")
+# ax1.set_ylabel("Loss")
+# ax1.set_xlabel("Epochs")
+#
+# # set the title, labels and accuracy axis limits of the validation data plot
+# ax2.set_title("Validation Loss")
+# ax2.set_ylabel("Loss")
+# ax2.set_xlabel("Epochs")
 
 
 # Instantiate a Keras tensor to allow us to build the model
 input_image = keras.Input(shape=(50, 50, 1))
 
 # layers for the encoder
-# x = Conv2D(filters=8, kernel_size=3, activation="relu", padding="same")(input_image)
-# encoded = MaxPooling2D(pool_size=2, padding="same")(x)
-#
-# # layers for the decoder
-# x = Conv2D(filters=8, kernel_size=3, activation="relu", padding="same")(encoded)
-# decoded = UpSampling2D(size=2)(x)
+x = Conv2D(filters=8, kernel_size=3, activation="relu", padding="same")(input_image)
+encoded = MaxPooling2D(pool_size=2, padding="same")(x)
 
-encoded = layers.Dense(25, activation="relu")(input_image)
-decoded = layers.Dense(1, activation="sigmoid")(encoded)
+# layers for the decoder
+x = Conv2D(filters=8, kernel_size=3, activation="relu", padding="same")(encoded)
+decoded = UpSampling2D(size=2)(x)
+
+# encoded = layers.Dense(25, activation="relu")(input_image)
+# decoded = layers.Dense(1, activation="sigmoid")(encoded)
 
 
 # create and compile the autoencoder model
@@ -109,9 +109,42 @@ autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
 # train the model
 model_data = autoencoder.fit(train_images, train_images, epochs=50, batch_size=1, validation_data=(test_images, test_images))
 
-training_loss = ax1.plot(model_data.history["loss"])
-validation_loss = ax2.plot(model_data.history["val_loss"])
+
+# plot the loss for the training and validation data
+# training_loss = ax1.plot(model_data.history["loss"])
+# validation_loss = ax2.plot(model_data.history["val_loss"])
+
+
+# create a subset of the validation data to reconstruct (first 10 images)
+images_to_reconstruct = test_images[:10]
+
+
+# number of images to reconstruct
+n = 10
+
+# reconstruct the images
+reconstructed_images = autoencoder.predict(test_images[:n])
+
+
+plt.figure(figsize=(20,4))
+for i in range(1, n+2):
+
+    # display the original images (with no axes)
+    ax_o = plt.subplot(2, n, i)
+    plt.imshow(test_images[i].reshape(50,50))
+    # plt.gray()
+    ax_o.get_xaxis().set_visible(False)
+    ax_o.get_yaxis().set_visible(False)
+
+    # display the reconstructed images (with no axes)
+    ax_r = plt.subplot(2, n, i)
+    plt.imshow(test_images[i].reshape(50, 50))
+    # plt.gray()
+    ax_r.get_xaxis().set_visible(False)
+    ax_r.get_yaxis().set_visible(False)
+
+
 
 plt.show()
-plt.savefig("Plots/autoencoder_loss_dense")
+plt.savefig("Plots/convoluted_autoencoder_reconstruction")
 
