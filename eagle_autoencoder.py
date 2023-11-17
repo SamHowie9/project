5f8b13e14069f1f3e6779ba1fc44dec7d836749f
Sam Howie <samhowie9@gmail.com>
2023-11-17 13:55:35 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -46,10 +46,10 @@ test_images = np.array(train_images[testing_count:])
 
 
 # Define keras tensor for the encoder
-input_image_encoder = keras.Input(shape=(256, 256, 3))                                                      # (256, 256, 3)
+input_image = keras.Input(shape=(256, 256, 3))                                                      # (256, 256, 3)
 
 # layers for the encoder
-x = Conv2D(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(input_image_encoder)    # (128, 128, 32)
+x = Conv2D(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(input_image)    # (128, 128, 32)
 x = Conv2D(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(x)                      # (64, 64, 64)
 x = Flatten()(x)                                                                                            # (262144) = (64 * 64 * 64)
 x = Dense(units=2048)(x)
