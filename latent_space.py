b67cb90704fda86de0834bea8ec3a4572f9be402
Sam Howie <samhowie9@gmail.com>
2023-11-17 11:55:22 +0000

    Code

--- latent_space.py
+++ latent_space.py
@@ -56,9 +56,9 @@ x = Reshape((64, 64, 32))(x)                                                    
 x = Conv2DTranspose(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(x)             # (128, 128, 64)
 x = Conv2DTranspose(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(x)             # (265, 256, 32)
 decoded = Conv2DTranspose(filters=3, kernel_size=3, activation="relu", padding="same")(x)                   # (256, 256, 3)
-
-# build the decoder
-decoder = keras.Model(input_image, decoded, name="decoder")
+#
+# # build the decoder
+# decoder = keras.Model(input_image, decoded, name="decoder")
 
 
 # print(encoder(image))
