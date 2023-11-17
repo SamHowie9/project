09fcf0a3120c3975e2b1d70f77290ca382b1100a
Sam Howie <samhowie9@gmail.com>
2023-11-17 12:29:11 +0000

    Code

--- latent_space.py
+++ latent_space.py
@@ -43,9 +43,9 @@ input_image_encoder = keras.Input(shape=(256, 256, 3))                          
 x = Conv2D(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(input_image_encoder)    # (128, 128, 32)
 x = Conv2D(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(x)                      # (64, 64, 64)
 x = Flatten()(x)                                                                                            # (262144) = (64 * 64 * 64)
-x = Dense(units=2048)(x)
-x = Dense(units=256)(x)
-x = Dense(units=32)(x)
+# x = Dense(units=2048)(x)
+# x = Dense(units=256)(x)
+# x = Dense(units=32)(x)
 encoded = Dense(units=2, activation="relu", name="z_mean")(x)                                                # (2)
 
 # build the encoder
@@ -55,10 +55,11 @@ encoder = keras.Model(inputs=input_image_encoder, outputs=encoded, name="encoder
 # input_image_decoder = keras.Input(shape=(2))                                                                # (2)
 
 # layers for the decoder
-x = Dense(units=32)(encoded)
-x = Dense(units=256)(x)
-x = Dense(units=2048)(x)
-x = Dense(units=64*64*32, activation="relu")(x)                                           # (131072) = (64 * 64 * 32)
+# x = Dense(units=32)(encoded)
+# x = Dense(units=256)(x)
+# x = Dense(units=2048)(x)
+# x = Dense(units=64*64*32, activation="relu")(x)                                           # (131072) = (64 * 64 * 32)
+x = Dense(units=64*64*32, activation="relu")(encoded)                                           # (131072) = (64 * 64 * 32)
 x = Reshape((64, 64, 32))(x)                                                                                # (64, 64, 32)
 x = Conv2DTranspose(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(x)             # (128, 128, 64)
 x = Conv2DTranspose(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(x)             # (265, 256, 32)
