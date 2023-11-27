2298bb8b111ece6bbab93b72b0b0bd875e67bfc7
Sam Howie <samhowie9@gmail.com>
2023-11-27 17:12:36 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -228,6 +228,7 @@ f2 = []
 f3 = []
 f4 = []
 f5 = []
+f6 = []
 
 # loop through each pair of values for each image and add the values to the individual lists
 for i in range(extracted_features.shape[0]):
@@ -243,7 +244,7 @@ for i in range(extracted_features.shape[0]):
 b_21, a_21 = np.polyfit(f2, f1, deg=1)
 b_31, a_31 = np.polyfit(f3, f1, deg=1)
 b_41, a_41 = np.polyfit(f4, f1, deg=1)
-b_51, a_45 = np.polyfit(f5, f1, deg=1)
+b_51, a_51 = np.polyfit(f5, f1, deg=1)
 b_32, a_32 = np.polyfit(f3, f2, deg=1)
 b_42, a_42 = np.polyfit(f4, f2, deg=1)
 b_43, a_43 = np.polyfit(f4, f3, deg=1)
@@ -261,9 +262,9 @@ sequence_f5 = np.linspace(np.min(f5), np.max(f5), num=100)
 
 
 # create the figure for the plot
-fig, axs = plt.subplots(3, 5, figsize=(25, 15))
+fig, axs = plt.subplots(4, 6, figsize=(35, 20))
 
-# pplot feature 1
+# plot feature 1
 axs[0][0].hist(f1, bins=40)
 axs[0][0].set_title("Feature 1")
 
@@ -307,7 +308,7 @@ axs[1][2].set_ylabel("Feature 1")
 
 # correlation between 5 and 1
 axs[1][3].scatter(f5, f1, s=5)
-axs[1][3].plot(sequence_f5, a_45 + b_51 * sequence_f5, color="k", lw=1.5)
+axs[1][3].plot(sequence_f5, a_51 + b_51 * sequence_f5, color="k", lw=1.5)
 axs[1][3].set_title("Feature 1 Against Feature 5")
 axs[1][3].set_xlabel("Feature 4")
 axs[1][3].set_ylabel("Feature 1")
@@ -349,7 +350,7 @@ axs[2][3].set_ylabel("Feature 5")
 
 # correlation between 5 and 4
 axs[2][4].scatter(f5, f4, s=5)
-axs[2][4].plot(sequence_f5, a_54 + b_54 * sequence_f4, color="k", lw=1.5)
+axs[2][4].plot(sequence_f5, a_54 + b_54 * sequence_f5, color="k", lw=1.5)
 axs[2][4].set_title("Feature 4 Against Feature 5")
 axs[2][4].set_xlabel("Feature 4")
 axs[2][4].set_ylabel("Feature 5")
