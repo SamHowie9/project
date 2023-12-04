4222a4625c6154525e955cf427dde0b36e05729c
Sam Howie <samhowie9@gmail.com>
2023-12-04 12:26:18 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -136,7 +136,7 @@ encoder_layer = autoencoder.get_layer("encoded")
 decoder_layer = autoencoder.get_layer("decoded")
 
 # get the shape of the decoder input
-decoder_input = keras.Input(shape=encoding_dim)
+decoder_input = keras.Input(shape=encoder_layer.shape)
 
 # build the encoder
 encoder = keras.Model(autoencoder.input, encoder_layer.output)
