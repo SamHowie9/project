21b39ccd70ddbdcf17472da341ea16d002bf978f
Sam Howie <samhowie9@gmail.com>
2023-12-06 14:39:58 +0000

    Code

--- k_clustering.py
+++ k_clustering.py
@@ -11,66 +11,43 @@ from sklearn.cluster import KMeans
 
 
 # set the encoding dimension (number of extracted features)
-encoding_dim = 9
+encoding_dim = 16
 
-# Define keras tensor for the encoder
-input_image = keras.Input(shape=(256, 256, 3))                                                      # (256, 256, 3)
-
-# layers for the encoder
-x = Conv2D(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(input_image)    # (128, 128, 64)
-x = Conv2D(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(x)              # (64, 128, 32)
-x = Conv2D(filters=16, kernel_size=3, strides=2, activation="relu", padding="same")(x)              # (32, 32, 16)
-x = Conv2D(filters=8, kernel_size=3, strides=2, activation="relu", padding="same")(x)               # (16, 16, 8)
-x = Conv2D(filters=4, kernel_size=3, strides=2, activation="relu", padding="same")(x)               # (8, 8, 4)
-x = Flatten()(x)                                                                                    # (256)
-x = Dense(units=32)(x)                                                                              # (32)
-encoded = Dense(units=encoding_dim, name="encoded")(x)                                                         # (2)
-
-
-# layers for the decoder
-x = Dense(units=32)(encoded)                                                                        # (32)
-x = Dense(units=256)(x)                                                                             # (256)
-x = Reshape((8, 8, 4))(x)                                                                           # (8, 8, 4)
-x = Conv2DTranspose(filters=4, kernel_size=3, strides=2, activation="relu", padding="same")(x)      # (16, 16, 4)
-x = Conv2DTranspose(filters=8, kernel_size=3, strides=2, activation="relu", padding="same")(x)      # (32, 32, 8)
-x = Conv2DTranspose(filters=16, kernel_size=3, strides=2, activation="relu", padding="same")(x)     # (64, 64, 16)
-x = Conv2DTranspose(filters=32, kernel_size=3, strides=2, activation="relu", padding="same")(x)     # (128, 128, 32)
-x = Conv2DTranspose(filters=64, kernel_size=3, strides=2, activation="relu", padding="same")(x)     # (256, 256, 64)
-decoded = Conv2DTranspose(filters=3, kernel_size=3, activation="sigmoid", padding="same", name="decoded")(x)        # (256, 256, 3)
-
-
-
-# crate autoencoder
-autoencoder = keras.Model(input_image, decoded)
-
-encoder = keras.Sequential()
-for i in range(0, 9):
-    encoder.add(autoencoder.layers[i])
-
-decoder = keras.Sequential()
-for i in range(9, 18):
-    decoder.add(autoencoder.layers[i])
-
-decoder.build(input_shape=(None, encoding_dim))
-
-
-
-
-# compile the autoencoder model
-autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
-
-# load the weights
-autoencoder.load_weights("Weights/9_feature_weights_new.h5")
 
 
 # load the extracted features
-extracted_features = np.load("Features/9_features_new.npy")
+extracted_features = np.load("Features/" + str(encoding_dim) + "_features_new.npy")
+
+
+
+# stores an empty list to contain all the image data to train the model
+all_images = []
+
+# load the supplemental file into a dataframe
+df = pd.read_csv("stab3510_supplemental_file/table1.csv", comment="#")
+
+print(df.shape)
+
+# loop through each galaxy in the supplmental file
+for i, galaxy in enumerate(df["GalaxyID"].tolist()):
+
+    filename = "galface_" + str(galaxy) + ".png"
+
+    image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/" + filename)
+    all_images.append(image)
+
+
+print(np.array(all_images).shape)
 
 
 
 
-kmeans = KMeans(n_clusters=9, random_state=0, n_init='auto')
+
+
+kmeans = KMeans(n_clusters=2, random_state=0, n_init='auto')
 clusters = kmeans.fit_predict(extracted_features)
+
+
 
 print(clusters)
 print(clusters.shape)
