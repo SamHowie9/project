9925850e8e7b324a1fee174ce7a28f297257a599
Sam Howie <samhowie9@gmail.com>
2023-12-06 10:49:35 +0000

    Code

--- property_comparison.py
+++ property_comparison.py
@@ -5,9 +5,14 @@ import pandas as pd
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D, Dense, Flatten, Reshape
 import keras
 import os
+from sklearn.cluster import KMeans
 
-# load the extracted features
-extracted_features = np.load("Features/8_features_new.npy")
+
+
+# # load the extracted features
+extracted_features = np.load("Features/9_features_new.npy")
+
+
 extracted_features = np.flipud(np.rot90(extracted_features))
 
 print(extracted_features.shape)
@@ -15,6 +20,22 @@ print(extracted_features.shape)
 # load the two excel files into dataframes
 df1 = pd.read_csv("stab3510_supplemental_file/table1.csv", comment="#")
 df2 = pd.read_csv("stab3510_supplemental_file/table2.csv", comment="#")
+
+
+# load the supplemental file into a dataframe
+data_df = pd.read_csv("stab3510_supplemental_file/table1.csv", comment="#")
+
+# loop through each galaxy in the supplmental file
+for i, galaxy in enumerate(data_df["GalaxyID"].tolist()):
+
+    filename = "galface_" + str(galaxy) + ".png"
+
+    if filename not in os.listdir("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/"):
+        df1.drop(axis=0, index=i)
+        df2.drop(axis=0, index=i)
+
+
+
 
 ab_magnitude = df1["galfit_mag"]
 mass = df2["galfit_lmstar"]
@@ -24,36 +45,46 @@ axis_ratio = (df1["galfit_q"] + df2["galfit_q"]) / 2
 position_angle = (df1["galfit_PA"] + df2["galfit_PA"]) / 2
 
 
-print(ab_magnitude[:-4].shape)
-print(extracted_features[0].shape)
 
-# print(df.to_string())
 
-# df = pd.DataFrame(columns=["AB Magnitude", "Stellar Mass", "Semi-Major Axis", "Sersic Index", "Axis Ratio", "Position Angle"])
-# # df = pd.DataFrame(dict("AB Magnitude"=ab_magnitude, "Stellar Mass"=mass, "Semi-Major Axis"=semi_major, "Sersic Index"=sersic, "Axis Ratio"=axis_ratio, "Position Angle"=position_angle))
-#
-# df["AB Magnitude"] = ab_magnitude
-# df["Stellar Mass"] = mass
-# df["Semi_Major Axis"] = semi_major
-# df["Sersic Index"] = sersic
-# df["Axis Ratio"] = axis_ratio
-# df["Position Angle"] = position_angle
+# load the extracted features
+extracted_features = np.load("Features/9_features_new.npy")
+
+# k means clustering for the extracted features
+kmeans = KMeans(n_clusters=9, random_state=0, n_init='auto')
+clusters = kmeans.fit_predict(extracted_features)
+
+
+# extracted_feature_df = pd.DataFrame(extracted_features, columns=["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9"])
+# extracted_feature_df["Category"] = clusters
+
 
 # print(df)
+#
+# kws = dict(s=5, linewidth=0)
+#
+# sns.pairplot(df, corner=True, hue="Category", plot_kws=kws, palette="colorblind")
+#
+# plt.savefig("Plots/9_feature_clustering_new")
+# plt.show()
 
-fig, axs = plt.subplots(6, 8, figsize=(25,20))
 
-for i in range(0, 8):
+
+
+
+fig, axs = plt.subplots(6, 9, figsize=(25,20))
+
+for i in range(0, 9):
 
     title = "Feature " + str(i+1)
     axs[0, i].set_title(title)
 
-    axs[0, i].scatter(extracted_features[i], ab_magnitude[:-4], s=2)
-    axs[1, i].scatter(extracted_features[i], mass[:-4], s=2)
-    axs[2, i].scatter(extracted_features[i], semi_major[:-4], s=2)
-    axs[3, i].scatter(extracted_features[i], sersic[:-4], s=2)
-    axs[4, i].scatter(extracted_features[i], axis_ratio[:-4], s=2)
-    axs[5, i].scatter(extracted_features[i], position_angle[:-4], s=2)
+    axs[0, i].scatter(extracted_features[i], ab_magnitude, s=2, hue=clusters)
+    axs[1, i].scatter(extracted_features[i], mass, s=2, hue=clusters)
+    axs[2, i].scatter(extracted_features[i], semi_major, s=2, hue=clusters)
+    axs[3, i].scatter(extracted_features[i], sersic, s=2, hue=clusters)
+    axs[4, i].scatter(extracted_features[i], axis_ratio, s=2, hue=clusters)
+    axs[5, i].scatter(extracted_features[i], position_angle, s=2, hue=clusters)
 
 axs[0, 0].set_ylabel("AB Magnitude")
 axs[1, 0].set_ylabel("Stellar Mass")
@@ -63,12 +94,12 @@ axs[4, 0].set_ylabel("Axis Ratio")
 axs[5, 0].set_ylabel("Position Angle")
 
 
-plt.savefig("Plots/8_feature_property_comparison")
+plt.savefig("Plots/9_feature_property_comparison_clusters")
 plt.show()
 
 
 
-# df = pd.DataFrame(extracted_features, columns=["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8"])
+# df = pd.DataFrame(extracted_features, columns=["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9"])
 #
 #
 # # create the pairplot with custom marker size
@@ -94,5 +125,5 @@ plt.show()
 # plt.subplots_adjust(hspace=0.2)
 #
 #
-# plt.savefig("Plots/8_feature_histogram_new")
+# plt.savefig("Plots/9_feature_histogram_new")
 # plt.show()
