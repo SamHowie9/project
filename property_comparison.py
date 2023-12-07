8c4c6e7e43d6d22883dc4eeb67462c8a43fb6654
Sam Howie <samhowie9@gmail.com>
2023-12-07 13:22:43 +0000

    Code

--- property_comparison.py
+++ property_comparison.py
@@ -39,49 +39,49 @@ print(centers.shape)
 
 
 
-# # load the two excel files into dataframes
-# df1 = pd.read_csv("stab3510_supplemental_file/table1.csv", comment="#")
-# df2 = pd.read_csv("stab3510_supplemental_file/table2.csv", comment="#")
-#
-#
-# # account for hte validation data and remove final 200 elements
-# df1.drop(df1.tail(200).index, inplace=True)
-# df2.drop(df2.tail(200).index, inplace=True)
-#
-#
-#
-# # extract relevant properties
-# galaxy_id = df1["GalaxyID"]
-# ab_magnitude = df1["galfit_mag"]
-# mass = df2["galfit_lmstar"]
-# semi_major = (df1["galfit_re"] + df2["galfit_re"]) / 2
-# sersic = (df1["galfit_n"] + df2["galfit_n"]) / 2
-# axis_ratio = (df1["galfit_q"] + df2["galfit_q"]) / 2
-# position_angle = (df1["galfit_PA"] + df2["galfit_PA"]) / 2
-#
-#
-# # create a new dataframe to contain all the relevant information about each galaxy
-# df = pd.DataFrame(columns=["GalaxyID", "galfit_mag", "galfit_lmstar", "galfit_re", "galfit_n", "galfit_q", "galfit_PA", "Cluster"])
-# df["GalaxyID"] = galaxy_id
-# df["galfit_mag"] = ab_magnitude
-# df["galfit_lmstar"] = mass
-# df["galfit_re"] = semi_major
-# df["galfit_n"] = sersic
-# df["galfit_q"] = axis_ratio
-# df["galfit_PA"] = position_angle
-#
-# print(df1.shape)
-# print(df2.shape)
-# print(df.shape)
-#
-# df["Cluster"] = clusters
-#
-#
-# group_1 = df.loc[df["Cluster"] == 0, "GalaxyID"].tolist()
-# group_2 = df.loc[df["Cluster"] == 1, "GalaxyID"].tolist()
-#
-# print(np.array(group_1).shape)
-# print(np.array(group_2).shape)
+# load the two excel files into dataframes
+df1 = pd.read_csv("stab3510_supplemental_file/table1.csv", comment="#")
+df2 = pd.read_csv("stab3510_supplemental_file/table2.csv", comment="#")
+
+
+# account for hte validation data and remove final 200 elements
+df1.drop(df1.tail(200).index, inplace=True)
+df2.drop(df2.tail(200).index, inplace=True)
+
+
+
+# extract relevant properties
+galaxy_id = df1["GalaxyID"]
+ab_magnitude = df1["galfit_mag"]
+mass = df2["galfit_lmstar"]
+semi_major = (df1["galfit_re"] + df2["galfit_re"]) / 2
+sersic = (df1["galfit_n"] + df2["galfit_n"]) / 2
+axis_ratio = (df1["galfit_q"] + df2["galfit_q"]) / 2
+position_angle = (df1["galfit_PA"] + df2["galfit_PA"]) / 2
+
+
+# create a new dataframe to contain all the relevant information about each galaxy
+df = pd.DataFrame(columns=["GalaxyID", "galfit_mag", "galfit_lmstar", "galfit_re", "galfit_n", "galfit_q", "galfit_PA", "Cluster"])
+df["GalaxyID"] = galaxy_id
+df["galfit_mag"] = ab_magnitude
+df["galfit_lmstar"] = mass
+df["galfit_re"] = semi_major
+df["galfit_n"] = sersic
+df["galfit_q"] = axis_ratio
+df["galfit_PA"] = position_angle
+
+print(df1.shape)
+print(df2.shape)
+print(df.shape)
+
+df["Cluster"] = clusters
+
+
+group_1 = df.loc[df["Cluster"] == 0, "GalaxyID"].tolist()
+group_2 = df.loc[df["Cluster"] == 1, "GalaxyID"].tolist()
+
+print(np.array(group_1).shape)
+print(np.array(group_2).shape)
 
 
 
