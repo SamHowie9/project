d58c7d6c84f8f44f7173cb67b7cc135a003d0e21
Sam Howie <samhowie9@gmail.com>
2023-12-08 16:44:43 +0000

    Code

--- property_comparison.py
+++ property_comparison.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import seaborn as sns
-# import pandas as pd
+# import seaborn as sns
+import pandas as pd
 # from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D, Dense, Flatten, Reshape
 # import keras
 import os
