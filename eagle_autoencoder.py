f28acd39878f8d3ec93423f3f3b74a242f2a82fa
Sam Howie <samhowie9@gmail.com>
2023-12-08 10:52:24 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -50,7 +50,7 @@ test_images = np.array(all_images[-200:])
 
 
 # set the encoding dimension (number of extracted features)
-encoding_dim = 32
+encoding_dim = 24
 
 
 
@@ -116,7 +116,7 @@ autoencoder.compile(optimizer="adam", loss=root_mean_squared_error)
 
 
 # train the model
-model_data = autoencoder.fit(train_images, train_images, epochs=5, batch_size=1, validation_data=(test_images, test_images))
+model_data = autoencoder.fit(train_images, train_images, epochs=200, batch_size=1, validation_data=(test_images, test_images))
 
 # save the weights
 autoencoder.save_weights(filepath="Weights/" + str(encoding_dim) + "_feature_weights.h5", overwrite=True)
@@ -139,48 +139,48 @@ np.save("Features/" + str(encoding_dim) + "_features.npy", extracted_features)
 
 
 
-# create a subset of the validation data to reconstruct (first 10 images)
-images_to_reconstruct = test_images[:10]
-
-# number of images to reconstruct
-n = 10
-
-# reconstruct the images
-reconstructed_images = autoencoder.predict(test_images[:n])
-
-# create figure to hold subplots
-fig, axs = plt.subplots(3, n-1, figsize=(20,5))
-
-# plot each subplot
-for i in range(0, n-1):
-
-    # show the original image (remove axes)
-    axs[0,i].imshow(test_images[i])
-    axs[0,i].get_xaxis().set_visible(False)
-    axs[0,i].get_yaxis().set_visible(False)
-
-    # show the reconstructed image (remove axes)
-    axs[1,i].imshow(reconstructed_images[i])
-    axs[1,i].get_xaxis().set_visible(False)
-    axs[1,i].get_yaxis().set_visible(False)
-
-    # calculate residue (difference between two images) and show this
-    residue_image = np.absolute(np.subtract(reconstructed_images[i], test_images[i]))
-    axs[2,i].imshow(residue_image)
-    axs[2,i].get_xaxis().set_visible(False)
-    axs[2,i].get_yaxis().set_visible(False)
-
-plt.savefig("Plots/" + str(encoding_dim) + "_feature_reconstruction")
-plt.show()
-
-
-
-
-
-# # plot the training and validation loss
-# plt.plot(model_data.history["loss"], label="training data")
-# plt.plot(model_data.history["val_loss"], label="validation data")
-# plt.legend()
+# # create a subset of the validation data to reconstruct (first 10 images)
+# images_to_reconstruct = test_images[:10]
 #
-# plt.savefig("Plots/" + str(encoding_dim) + "_feature_loss")
+# # number of images to reconstruct
+# n = 10
+#
+# # reconstruct the images
+# reconstructed_images = autoencoder.predict(test_images[:n])
+#
+# # create figure to hold subplots
+# fig, axs = plt.subplots(3, n-1, figsize=(20,5))
+#
+# # plot each subplot
+# for i in range(0, n-1):
+#
+#     # show the original image (remove axes)
+#     axs[0,i].imshow(test_images[i])
+#     axs[0,i].get_xaxis().set_visible(False)
+#     axs[0,i].get_yaxis().set_visible(False)
+#
+#     # show the reconstructed image (remove axes)
+#     axs[1,i].imshow(reconstructed_images[i])
+#     axs[1,i].get_xaxis().set_visible(False)
+#     axs[1,i].get_yaxis().set_visible(False)
+#
+#     # calculate residue (difference between two images) and show this
+#     residue_image = np.absolute(np.subtract(reconstructed_images[i], test_images[i]))
+#     axs[2,i].imshow(residue_image)
+#     axs[2,i].get_xaxis().set_visible(False)
+#     axs[2,i].get_yaxis().set_visible(False)
+#
+# plt.savefig("Plots/" + str(encoding_dim) + "_feature_reconstruction")
 # plt.show()
+
+
+
+
+
+# plot the training and validation loss
+plt.plot(model_data.history["loss"], label="training data")
+plt.plot(model_data.history["val_loss"], label="validation data")
+plt.legend()
+
+plt.savefig("Plots/" + str(encoding_dim) + "_feature_loss")
+plt.show()
