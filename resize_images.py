3f4d911cf5e45511e49d7d8cff3273eac405db44
Sam Howie <samhowie9@gmail.com>
2023-12-15 13:57:22 +0000

    Code

--- resize_images.py
+++ resize_images.py
@@ -81,31 +81,68 @@ for galaxy in galaxies:
     filename = "galface_" + galaxy + ".png"
 
     # open the image and append it to the main list
-    image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/" + filename)
-    chosen_images.append(image)
+    # image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/" + filename)
+
+    # chosen_images.append(image)
 
 chosen_images = np.array(chosen_images)
 
 
 
 
-fig, axs = plt.subplots(2, 10, figsize=(28, 6))
+fig, axs = plt.subplots(3, 10, figsize=(28, 6))
 
 for i in range(len(galaxies)):
 
-    axs[0, i].imshow(chosen_images[i])
+    # axs[0, i].imshow(chosen_images[i])
     axs[0, i].axvline(x=64, c="white")
     axs[0, i].axvline(x=192, c="white")
+    axs[0, i].axhline(y=64, c="white")
+    axs[0, i].axhline(y=192, c="white")
     axs[0, i].get_xaxis().set_visible(False)
     axs[0, i].get_yaxis().set_visible(False)
 
-    intensity = chosen_images[i].mean(axis=2).mean(axis=0)
-    axs[1, i].bar(x=range(0, len(intensity)), height=intensity, width=1)
+    cutoff = 0.06
+
+
+    intensity_x = chosen_images[i].mean(axis=2).mean(axis=0)
+    axs[1, i].bar(x=range(0, len(intensity_x)), height=intensity, width=1)
     axs[1, i].axvline(x=64, c="black")
     axs[1, i].axvline(x=192, c="black")
+    axs[1, i].axhline(y=0.06, c="black", alpha=0.2)
     axs[1, i].get_xaxis().set_visible(False)
     axs[1, i].set_ylim([0, 0.35])
-    # axs[1, i].get_yaxis().set_visible(False)
+
+    intensity_y = chosen_images[i].mean(axis=2).mean(axis=1)
+    axs[2, i].bar(x=range(0, len(intensity_y)), height=intensity, width=1)
+    axs[2, i].axvline(x=64, c="black")
+    axs[2, i].axvline(x=192, c="black")
+    axs[2, i].axhline(y=0.06, c="black", alpha=0.2)
+    axs[2, i].get_xaxis().set_visible(False)
+    axs[2, i].set_ylim([0, 0.35])
+
+    start_x = 0
+    start_y = 0
+    end_x = 255
+    end_y = 0
+
+    for j in range(0, 128):
+
+        if intensity_x[j] <= cutoff:
+            start = intensity_x[j]
+        if intensity_x[-j] <= cutoff:
+            end = intensity_x[-j]
+
+        if intensity_y[j] <= cutoff:
+            start = intensity_y[j]
+        if intensity_y[-j] <= cutoff:
+            end = intensity_y[-j]
+
+    axs[0, i].axvline(x=start_x, c="blue")
+    axs[0, i].axline(x=end_x, c="blue")
+
+    axs[0, i].axhline(x=start_y, c="blue")
+    axs[0, i].axhline(x=end_y, c="blue")
 
 
 
