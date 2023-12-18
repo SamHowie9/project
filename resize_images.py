00b2d683e2e5f2bf1edcad753e485a67ef512671
Sam Howie <samhowie9@gmail.com>
2023-12-18 15:40:21 +0000

    Code

--- resize_images.py
+++ resize_images.py
@@ -121,6 +121,8 @@ def resize_image(image, cutoff):
     intensity_x = image.mean(axis=2).mean(axis=0)
     intensity_y = image.mean(axis=2).mean(axis=1)
 
+    half_max_intensity_x = np.max(intensity_x)/2
+    half_max_intensity_y = np.max(intensity_y)/2
 
     size = len(intensity_x)
 
@@ -129,17 +131,36 @@ def resize_image(image, cutoff):
     end_x = 255
     end_y = 255
 
-    found_start_x = 0
-    found_start_y = 0
-    found_end_x = 0
-    found_end_y = 0
+    found_start_x = False
+    found_start_y = False
+    found_end_x = False
+    found_end_y = False
 
-    # # loop through half of the image
-    # for j in range(0, int(size/2)):
-    #
-    #     # check if we are below the cutoff, if so, increment the pointer
-    #     if (intensity_x[j]/mean_intensity > )
+    # loop through half of the image
+    for j in range(0, int(size/2)):
 
+        # if we haven't previously found the cutoff point and are still below the cutoff, increment the pointer
+        if found_start_x is False and intensity_x[j]/mean_intensity > half_max_intensity_x:
+            start_x += 1
+        else:
+            found_start_x = True
+
+        if found_end_x is False and intensity_x[j]/mean_intensity > half_max_intensity_x:
+            end_x -= 1
+        else:
+            found_end_x = True
+
+        if found_start_y is False and intensity_y[j]/mean_intensity > half_max_intensity_y:
+            start_x += 1
+        else:
+            found_start_y = True
+
+        if found_end_y is False and intensity_y[j]/mean_intensity > half_max_intensity_y:
+            end_y -= 1
+        else:
+            found_end_y = True
+
+    return start_x, end_x
 
     # for j in range(0, int(size/2)):
     #
@@ -158,18 +179,18 @@ def resize_image(image, cutoff):
     #     if (intensity_y[-j] > cutoff) and (found_end_y == 0):
     #         end_y = 255 - j
     #         found_end_y = 1
-
-
-    # check if image is too large to crop, if no we have to scale it down to 128, 128
-    if start_x < 64 and start_y < 64 and end_x > 192 and end_y > 192:
-        image = cv2.resize(image, (128, 128))
-
-    # if the image isn't too large, we can do a center crop
-    else:
-        image = center_crop(image, (128, 128))
-
-    print()
-    return image
+    #
+    #
+    # # check if image is too large to crop, if no we have to scale it down to 128, 128
+    # if start_x < 64 and start_y < 64 and end_x > 192 and end_y > 192:
+    #     image = cv2.resize(image, (128, 128))
+    #
+    # # if the image isn't too large, we can do a center crop
+    # else:
+    #     image = center_crop(image, (128, 128))
+    #
+    # print()
+    # return image
 
 
 
@@ -201,21 +222,32 @@ for i in range(len(galaxies)):
     #
     #
 
-    mean_intensity = chosen_images[i].mean()
+    mean_intensity = image.mean()
 
-    intensity_x = chosen_images[i].mean(axis=2).mean(axis=0)
-    axs[1, i].bar(x=range(0, len(intensity_x)), height=intensity_x, width=1)
-    # axs[1, i].axvline(x=64, c="black")
-    # axs[1, i].axvline(x=192, c="black")
-    # axs[1, i].axhline(y=0.06, c="black", alpha=0.2)
-    axs[1, i].get_xaxis().set_visible(False)
-    # axs[1, i].set_ylim([0, 0.35])
+    intensity_x = image.mean(axis=2).mean(axis=0)
+    intensity_y = image.mean(axis=2).mean(axis=1)
 
-    axs[2, i].bar(x=range(0, len(intensity_x)), height=intensity_x/mean_intensity, width=1)
+    half_max_intensity_x = np.max(intensity_x) / 2
+    half_max_intensity_y = np.max(intensity_y) / 2
+
+    x_min, x_max = resize_image(chosen_images[i])
+
+    # # intensity_x = chosen_images[i].mean(axis=2).mean(axis=0)
+    # axs[1, i].bar(x=range(0, len(intensity_x)), height=intensity_x, width=1)
+    # # axs[1, i].axvline(x=64, c="black")
+    # # axs[1, i].axvline(x=192, c="black")
+    # # axs[1, i].axhline(y=0.06, c="black", alpha=0.2)
+    # axs[1, i].get_xaxis().set_visible(False)
+    # # axs[1, i].set_ylim([0, 0.35])
+
+    axs[1, i].bar(x=range(0, len(intensity_x)), height=intensity_x/mean_intensity, width=1)
     # axs[2, i].axvline(x=64, c="black")
     # axs[2, i].axvline(x=192, c="black")
     # axs[2, i].axhline(y=0.06, c="black", alpha=0.2)
-    axs[2, i].get_xaxis().set_visible(False)
+    axs[1, i].get_xaxis().set_visible(False)
+    axs[1, i].axvspan(x_min, x_msx, facecolour="yellow", alpha=0.5)
+    axs[1, i].axvline(x=64, c="black")
+    axs[1, i].axvline(x=192, c="black")
     # axs[2, i].set_ylim([0, 0.35])
 
 
