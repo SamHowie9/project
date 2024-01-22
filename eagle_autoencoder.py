dceed9d86dff31f846ed9e90a8708fc7caea45ec
Sam Howie <samhowie9@gmail.com>
2024-01-22 15:19:05 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -15,7 +15,7 @@ import cv2
 
 
 # select which GPU to use
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 
 
@@ -137,7 +137,7 @@ test_images = np.array(all_images[-200:])
 
 
 # set the encoding dimension (number of extracted features)
-encoding_dim = 19
+encoding_dim = 18
 
 
 # Define keras tensor for the encoder
