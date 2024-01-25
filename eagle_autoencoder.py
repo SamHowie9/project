032e928e088fde7cbab9743dcbb1e1ecb8bc9dfb
Sam Howie <samhowie9@gmail.com>
2024-01-25 11:28:34 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -15,7 +15,7 @@ import cv2
 
 
 # select which GPU to use
-os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "8"
 
 
 def center_crop(img, dim):
@@ -136,7 +136,7 @@ test_images = np.array(all_images[-200:])
 
 
 # set the encoding dimension (number of extracted features)
-encoding_dim = 5
+encoding_dim = 4
 
 
 
