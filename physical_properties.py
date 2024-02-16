3e251c09291141fbdc2861095c6369c067925375
Sam Howie <samhowie9@gmail.com>
2024-02-16 14:39:03 +0000

    Code

--- physical_properties.py
+++ physical_properties.py
@@ -25,7 +25,7 @@ pd.set_option('display.width', 500)
 encoding_dim = 44
 
 # set the number of clusters
-n_clusters = 10
+n_clusters = 12
 
 
 # load the extracted features
@@ -59,15 +59,16 @@ print(structure_properties)
 
 
 physical_properties["Log_Stellar_Mass"] = np.log10(physical_properties["MassType_Star"])
+physical_properties["Log_DM_Mass"] = np.log10(physical_properties["MassType_DM"])
 physical_properties["Stellar_Mass/DM_Mass"] = physical_properties["MassType_Star"]/physical_properties["MassType_DM"]
 
 
-med_physical_properties = pd.DataFrame(columns=["MassType_Star", "MassType_Gas", "MassType_DM", "MassType_BH", "BlackHoleMass", "InitialMassWeightedStellarAge", "StarFormationRate", "Log_Stellar_Mass", "Stellar_Mass/DM_Mass"])
+med_physical_properties = pd.DataFrame(columns=["MassType_Star", "MassType_Gas", "MassType_DM", "MassType_BH", "BlackHoleMass", "InitialMassWeightedStellarAge", "StarFormationRate", "Log_Stellar_Mass", "Log_DM_Mass", "Stellar_Mass/DM_Mass"])
 med_structure_properties = pd.DataFrame(columns=["n_r", "q_r", "re_r", "mag_r"])
 
 
 for i in range(0, n_clusters):
-    med_physical_cluster = physical_properties.loc[physical_properties["Cluster"] == i, ["MassType_Star", "MassType_Gas", "MassType_DM", "MassType_BH", "BlackHoleMass", "InitialMassWeightedStellarAge", "StarFormationRate", "Log_Stellar_Mass", "Stellar_Mass/DM_Mass"]].median()
+    med_physical_cluster = physical_properties.loc[physical_properties["Cluster"] == i, ["MassType_Star", "MassType_Gas", "MassType_DM", "MassType_BH", "BlackHoleMass", "InitialMassWeightedStellarAge", "StarFormationRate", "Log_Stellar_Mass", "Log_DM_Mass", "Stellar_Mass/DM_Mass"]].median()
     med_physical_properties.loc[i] = med_physical_cluster.tolist()
 
     med_structure_cluster = structure_properties.loc[structure_properties["Cluster"] == i, ["n_r", "q_r", "re_r", "mag_r"]].median()
@@ -85,8 +86,8 @@ print(med_structure_properties)
 
 
 
-# sns.scatterplot(data=physical_properties, x="Log_Stellar_Mass", y="Stellar_Mass/DM_Mass", hue=structure_properties["re_r"])
-sns.scatterplot(data=med_physical_properties, x="Log_Stellar_Mass", y="Stellar_Mass/DM_Mass", hue=med_structure_properties["re_r"])
+# sns.scatterplot(data=physical_properties, x="Log_Stellar_Mass", y="Stellar_Mass/DM_Mass", hue=structure_properties["n_r"])
+# sns.scatterplot(data=med_physical_properties, x="Log_Stellar_Mass", y="Stellar_Mass/DM_Mass", hue=med_structure_properties["re_r"])
 # plt.ylim(-0.1, 3)
 
 # sns.histplot(data=physical_properties, x="Stellar_Mass/DM_Mass")
@@ -94,4 +95,25 @@ sns.scatterplot(data=med_physical_properties, x="Log_Stellar_Mass", y="Stellar_M
 
 # plt.scatter(physical_properties["MassType_Star"], physical_properties["MassType_Star"])
 
+plt.figure(figsize=(10, 8))
+
+# plt.scatter(x=med_physical_properties["Log_Stellar_Mass"], y=med_physical_properties["Stellar_Mass/DM_Mass"], c=med_structure_properties["n_r"], cmap="inferno_r", s=150, ec="black", lw=0.5)
+plt.scatter(x=med_physical_properties["Log_DM_Mass"], y=med_physical_properties["Stellar_Mass/DM_Mass"], c=med_structure_properties["n_r"], cmap="inferno_r", s=150, ec="black", lw=0.5)
+
+
+cbar = plt.colorbar()
+cbar.set_label(label="Sersic Index", size=18, labelpad=20)
+cbar.ax.tick_params(labelsize=12)
+
+
+plt.ylim(0.01, 0.05)
+plt.xlim(11.65, 12.3)
+
+# plt.xlabel("Log(Stellar Mass)", fontsize=18, labelpad=20)
+plt.xlabel("Log(Dark Matter Halo Mass)", fontsize=18, labelpad=20)
+plt.ylabel("Stellar Mass / Dark Matter Halo Mass", fontsize=18, labelpad=20)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+
+plt.savefig("Plots/Stellar_Mass_DM_Mass_Sersic_crop")
 plt.show()
