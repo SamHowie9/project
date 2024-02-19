3b5ce429389992355d70f197178b1f8f11c1fdac
Sam Howie <samhowie9@gmail.com>
2024-02-19 15:11:00 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -17,9 +17,9 @@ plt.switch_backend('agg')
 
 
 # select which GPU to use
-os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 
-encoding_dim = 31
+encoding_dim = 32
 
 
 def center_crop(img, dim):
