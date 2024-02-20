753f494916a5876f086849cc0d650e058ff4ad12
Sam Howie <samhowie9@gmail.com>
2024-02-20 14:15:12 +0000

    Code

--- eagle_autoencoder.py
+++ eagle_autoencoder.py
@@ -17,9 +17,9 @@ plt.switch_backend('agg')
 
 
 # select which GPU to use
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 
-encoding_dim = 35
+encoding_dim = 45
 
 
 
