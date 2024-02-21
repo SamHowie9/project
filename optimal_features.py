0f65c43e0629603e3e511d77f7b7d94ec7aff26f
Sam Howie <samhowie9@gmail.com>
2024-02-21 14:52:48 +0000

    Code

--- optimal_features.py
+++ optimal_features.py
@@ -57,65 +57,66 @@ max_val_loss = []
 min_val_loss = []
 
 # 16, 26, 36    7, 9, 13, 20, 21, 22, 25, 30, 38, 41
-# 25, 35, 45    1, 6, 7, 8, 27
+# 25, 35, 45    1, 8, 27, 35
 
 
 for i in range(1, 46):
-    feature_loss = np.load("Loss/" + str(i) + "_feature_loss.npy")
 
-    # loss.append(feature_loss[0])
-    # val_loss.append(feature_loss[1])
+    feature_loss = np.load("Loss/" + str(i) + "_feature_loss_3.npy")
 
-    feature_loss_1 = np.load("Loss/" + str(i) + "_feature_loss.npy")
-    feature_loss_2 = np.load("Loss/" + str(i) + "_feature_loss_2.npy")
-    feature_loss_3 = np.load("Loss/" + str(i) + "_feature_loss_3.npy")
+    loss.append(feature_loss[0])
+    val_loss.append(feature_loss[1])
 
-    med_loss.append(np.median((feature_loss_1[0], feature_loss_2[0], feature_loss_3[0])))
-    max_loss.append(max(feature_loss_1[0], feature_loss_2[0], feature_loss_3[0]))
-    min_loss.append(min(feature_loss_1[0], feature_loss_2[0], feature_loss_3[0]))
-
-    med_val_loss.append(np.median((feature_loss_1[1], feature_loss_2[1], feature_loss_3[1])))
-    max_val_loss.append(max(feature_loss_1[1], feature_loss_2[1], feature_loss_3[1]))
-    min_val_loss.append(min(feature_loss_1[1], feature_loss_2[1], feature_loss_3[1]))
-
-
-loss_err = []
-val_loss_err = []
-
-for i in range(len(med_loss)):
-
-    loss_err.append([(med_loss[i] - min_loss[i]), (max_loss[i] - med_loss[i])])
-    val_loss_err.append([(med_val_loss[i] - min_val_loss[i]), (max_val_loss[i] - med_val_loss[i])])
-
-loss_err = np.array(loss_err).T
-val_loss_err = np.array(val_loss_err).T
-
-print(loss_err)
-print(val_loss_err)
-
-
-plt.scatter(x=range(1, 46), y=med_loss, label="Training Images")
-plt.errorbar(x=range(1, 46), y=med_loss, yerr=loss_err, ls="none")
-
-plt.scatter(x=range(1, 46), y=med_val_loss, label="Validation Images")
-plt.errorbar(x=range(1, 46), y=med_val_loss, yerr=val_loss_err, ls="none")
-
-plt.show()
-
-
-# plt.scatter(x=range(1, 25), y=loss, label="Training Images")
-# plt.scatter(x=range(1, 25), y=val_loss, label="Validation Images")
+#     feature_loss_1 = np.load("Loss/" + str(i) + "_feature_loss.npy")
+#     feature_loss_2 = np.load("Loss/" + str(i) + "_feature_loss_2.npy")
+#     feature_loss_3 = np.load("Loss/" + str(i) + "_feature_loss_3.npy")
 #
-# # plt.plot(range(17, 41), loss)
-# # plt.plot(range(17, 41), val_loss)
+#     med_loss.append(np.median((feature_loss_1[0], feature_loss_2[0], feature_loss_3[0])))
+#     max_loss.append(max(feature_loss_1[0], feature_loss_2[0], feature_loss_3[0]))
+#     min_loss.append(min(feature_loss_1[0], feature_loss_2[0], feature_loss_3[0]))
 #
-# plt.xlabel("Number of Extracted Features")
-# plt.ylabel("Root-Mean-Squared Error")
+#     med_val_loss.append(np.median((feature_loss_1[1], feature_loss_2[1], feature_loss_3[1])))
+#     max_val_loss.append(max(feature_loss_1[1], feature_loss_2[1], feature_loss_3[1]))
+#     min_val_loss.append(min(feature_loss_1[1], feature_loss_2[1], feature_loss_3[1]))
 #
-# plt.legend(bbox_to_anchor=(0., 1.00, 1., .100), loc='lower center', ncol=2)
 #
-# # plt.savefig("Plots/extracted_feat_vs_loss")
+# loss_err = []
+# val_loss_err = []
+#
+# for i in range(len(med_loss)):
+#
+#     loss_err.append([(med_loss[i] - min_loss[i]), (max_loss[i] - med_loss[i])])
+#     val_loss_err.append([(med_val_loss[i] - min_val_loss[i]), (max_val_loss[i] - med_val_loss[i])])
+#
+# loss_err = np.array(loss_err).T
+# val_loss_err = np.array(val_loss_err).T
+#
+# # print(loss_err)
+# # print(val_loss_err)
+#
+#
+# plt.scatter(x=range(1, 46), y=med_loss, label="Training Images")
+# plt.errorbar(x=range(1, 46), y=med_loss, yerr=loss_err, ls="none")
+#
+# plt.scatter(x=range(1, 46), y=med_val_loss, label="Validation Images")
+# plt.errorbar(x=range(1, 46), y=med_val_loss, yerr=val_loss_err, ls="none")
+#
 # plt.show()
+
+
+plt.scatter(x=range(1, 46), y=loss, label="Training Images")
+plt.scatter(x=range(1, 46), y=val_loss, label="Validation Images")
+
+# plt.plot(range(17, 41), loss)
+# plt.plot(range(17, 41), val_loss)
+
+plt.xlabel("Number of Extracted Features")
+plt.ylabel("Root-Mean-Squared Error")
+
+plt.legend(bbox_to_anchor=(0., 1.00, 1., .100), loc='lower center', ncol=2)
+
+# plt.savefig("Plots/extracted_feat_vs_loss")
+plt.show()
 
 
 
@@ -155,7 +156,7 @@ for encoding_dim in range(1, 46):
     correlation_df_2 = pd.DataFrame(columns=all_properties.columns[1:])
     correlation_df_3 = pd.DataFrame(columns=all_properties.columns[1:])
 
-    print(correlation_df_1)
+    # print(correlation_df_1)
 
     for feature in range(0, len(extracted_features_switch)):
 
@@ -187,7 +188,7 @@ for encoding_dim in range(1, 46):
         # add the correlation of that feature to the main dataframe
         # structure_correlation_df.loc[len(structure_correlation_df)] = correlation_list
 
-        print(correlation_list_1)
+        # print(correlation_list_1)
 
         correlation_df_1.loc[len(correlation_df_1)] = correlation_list_1
         correlation_df_2.loc[len(correlation_df_2)] = correlation_list_2
@@ -215,9 +216,9 @@ for encoding_dim in range(1, 46):
 
 
 
-print(med_relevant_feature_number)
-print(max_relevant_feature_number)
-print(min_relevant_feature_number)
+# print(med_relevant_feature_number)
+# print(max_relevant_feature_number)
+# print(min_relevant_feature_number)
 
 relevant_err = []
 ratio_err = []
