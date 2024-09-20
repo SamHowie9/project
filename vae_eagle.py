db04b68e9fd0afb8e56f9d2448936cde880bed23
Sam <samhowie9@gmail.com>
2024-09-20 12:12:33 +0100

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -15,11 +15,11 @@ from matplotlib import image as mpimg
 # tf.config.list_physical_devices('GPU')
 
 
-encoding_dim = 38
+encoding_dim = 40
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="5"
+os.environ["CUDA_VISIBLE_DEVICES"]="6"
 
 
 
