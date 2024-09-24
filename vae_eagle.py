c2e54805cb335f8c66a3c9386e759772b43f1bd2
Sam <samhowie9@gmail.com>
2024-09-24 14:22:29 +0100

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -19,10 +19,10 @@ encoding_dim = 30
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="8"
+os.environ["CUDA_VISIBLE_DEVICES"]="9"
 
 # number of epochs for run
-epochs = 350
+epochs = 400
 
 
 
