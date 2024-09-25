c86dabd671112364db960dca025c374d0ee3af93
Sam <samhowie9@gmail.com>
2024-09-25 15:05:15 +0100

    Code

--- tng_images.py
+++ tng_images.py
@@ -4,4 +4,8 @@ import pandas as pd
 
 f = h5py.File("/cosma7/data/durham/dc-howi1/project/TNG100/Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc_091.hdf5", "r")
 
-print(list(f.keys()))
+images = pd.DataFrame(f["Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc"])
+
+print(images)
+
+# print(list(f.keys()))
