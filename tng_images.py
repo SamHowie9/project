653af48da501017e1f025d1d0ac0282d74ef2eda
Sam <samhowie9@gmail.com>
2024-09-27 14:08:55 +0100

    Code

--- tng_images.py
+++ tng_images.py
@@ -4,21 +4,25 @@ import pandas as pd
 
 f = h5py.File("/cosma7/data/durham/dc-howi1/project/TNG100/Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc_091.hdf5", "r")
 
+print(f.keys())
+
 # images = pd.DataFrame(f["Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc"])
 
-images = np.array(f["Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc"])
-
-face_on = np.transpose(np.transpose(images)[0])
-
-first_image_all_bands = face_on[0]
-
-first_image_ugr = first_image_all_bands[0:3]
-
-
-print(images.shape)
-print(face_on.shape)
-print(first_image_all_bands)
-print(first_image_ugr.shape)
+# indices = np.array(f)
+#
+# images = np.array(f["Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc"])
+#
+# face_on = np.transpose(np.transpose(images)[0])
+#
+# first_image_all_bands = face_on[0]
+#
+# first_image_ugr = first_image_all_bands[0:3]
+#
+#
+# print(images.shape)
+# print(face_on.shape)
+# print(first_image_all_bands)
+# print(first_image_ugr.shape)
 
 
 # print(np.array(f["Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc"]).shape)
