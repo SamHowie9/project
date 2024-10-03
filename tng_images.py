860cfdc2b37f99fab980a238e1db013c50a2d091
Sam <samhowie9@gmail.com>
2024-10-03 11:17:24 +0100

    Code

--- tng_images.py
+++ tng_images.py
@@ -8,6 +8,8 @@ file = tarfile.open("/cosma7/data/durham/dc-howi1/project/TNG100/sdss_095.tar")
 
 print(file.getnames())
 
+file.extractall()
+
 
 
 # f = h5py.File("/cosma7/data/durham/dc-howi1/project/TNG100/Subhalo_StellarPhot_p07c_cf00dust_res_conv_ns1_rad30pkpc_091.hdf5", "r")
