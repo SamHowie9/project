0eb24cb615fdcca39b7fe1083470fc6d2bb823d1
Sam <samhowie9@gmail.com>
2024-10-09 16:31:36 +0100

    Code

--- tng_images.py
+++ tng_images.py
@@ -6,19 +6,21 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
-file = tarfile.open("/cosma7/data/durham/dc-howi1/project/TNG100/sdss_095.tar")
+# file = tarfile.open("/cosma7/data/durham/dc-howi1/project/TNG100/sdss_095.tar")
+#
+# # for member in file.getmembers():
+# #     if member.name.startswith("sdss/snapnum_095/data/"):
+# #         print(member.name)
+#
+# # file.extractall(path="sdss/snapnum_095/data/")
+#
+# file.extract("sdss/snapnum_095/data/broadband_540856.fits")
+# file.extract("sdss/snapnum_095/data/broadband_546348.fits")
+# file.extract("sdss/snapnum_095/data/broadband_166270.fits")
+# file.extract("sdss/snapnum_095/data/broadband_247336.fits")
+# file.extract("sdss/snapnum_095/data/broadband_391637.fits")
 
-# for member in file.getmembers():
-#     if member.name.startswith("sdss/snapnum_095/data/"):
-#         print(member.name)
 
-# file.extractall(path="sdss/snapnum_095/data/")
-
-file.extract("sdss/snapnum_095/data/broadband_540856.fits")
-file.extract("sdss/snapnum_095/data/broadband_546348.fits")
-file.extract("sdss/snapnum_095/data/broadband_166270.fits")
-file.extract("sdss/snapnum_095/data/broadband_247336.fits")
-file.extract("sdss/snapnum_095/data/broadband_391637.fits")
 
 hdu_list_1 = fits.open("sdss/snapnum_095/data/broadband_540856.fits")
 image_1 = hdu_list_1[0].data
@@ -40,35 +42,44 @@ hdu_list_5 = fits.open("sdss/snapnum_095/data/broadband_391637.fits")
 image_5 = hdu_list_5[0].data
 image_5 = np.array(image_5[0:3]).T
 
+
+df = pd.DataFrame(np.array(hdu_list_1[0].data)[0])
+print(df)
+
+
 # plt.imshow(image)
 
-fig, axs = plt.subplots(5, 1)
-
-axs[0].imshow(image_1)
-axs[1].imshow(image_2)
-axs[2].imshow(image_3)
-axs[3].imshow(image_4)
-axs[4].imshow(image_5)
-
-for i in range(0, 5):
-    axs[i].get_yaxis().set_visible(False)
-    axs[i].get_xaxis().set_visible(False)
-
-
-
-
-# fig, axs = plt.subplots(4, 1)
-# axs[0].imshow(image[0])
-# axs[1].imshow(image[1])
-# axs[2].imshow(image[2])
-# axs[3].imshow(image[3])
+# fig, axs = plt.subplots(1, 5, figsize=(25, 5))
 #
-# for i in range(0, 4):
-#     axs[i].get_xaxis().set_visible(False)
+# axs[0].imshow(image_1)
+# axs[1].imshow(image_2)
+# axs[2].imshow(image_3)
+# axs[3].imshow(image_4)
+# axs[4].imshow(image_5)
+#
+# for i in range(0, 5):
 #     axs[i].get_yaxis().set_visible(False)
+#     axs[i].get_xaxis().set_visible(False)
+#
+# # fig, axs = plt.subplots(4, 1)
+# # axs[0].imshow(image[0])
+# # axs[1].imshow(image[1])
+# # axs[2].imshow(image[2])
+# # axs[3].imshow(image[3])
+# #
+# # for i in range(0, 4):
+# #     axs[i].get_xaxis().set_visible(False)
+# #     axs[i].get_yaxis().set_visible(False)
+#
+# plt.savefig("Variational TNG/Plots/tng_test")
+# plt.show()
 
-plt.savefig("Variational TNG/Plots/tng_test")
-plt.show()
+
+
+
+
+
+
 
 # file.extractall()
 
