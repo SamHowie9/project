9ae733ced31014d8836f2a1849b0c8b30535e776
Sam <samhowie9@gmail.com>
2024-11-11 13:40:02 +0000

    Code

---
+++ pca.py
@@ -0,0 +1,12 @@
+from sklearn.decomposition import PCA
+import numpy as np
+import pandas as pd
+
+
+extracted_features = np.load("Variational Eagle/Extracted Features/Normalised to G/15_feature_300_epoch_features_1.npy")[0]
+
+print(extracted_features.shape)
+
+pca = PCA(n_components=0.5).fit(extracted_features.T)
+
+print(pca.components_.shape)
