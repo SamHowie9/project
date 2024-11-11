7e54a1f08596c32bd10853eed6ce540adec7a3d4
Sam <samhowie9@gmail.com>
2024-11-11 15:26:13 +0000

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -15,11 +15,11 @@ from matplotlib import image as mpimg
 # tf.config.list_physical_devices('GPU')
 
 
-encoding_dim = 29
+encoding_dim = 28
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="6"
+os.environ["CUDA_VISIBLE_DEVICES"]="7"
 
 # number of epochs for run
 epochs = 300
