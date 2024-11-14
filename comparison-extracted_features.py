1dbdc623e369650346de23317b99d9537a9887ab
Sam <samhowie9@gmail.com>
2024-11-14 15:36:34 +0000

    Code

--- comparison-extracted_features.py
+++ comparison-extracted_features.py
@@ -17,10 +17,10 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 
 
-encoding_dim = 18
+encoding_dim = 15
 
 # load the extracted features
-extracted_features = np.load("Variational Eagle/Extracted Features/Normalised Individually/" + str(encoding_dim) + "_feature_300_epoch_features_1.npy")[0]
+extracted_features = np.load("Variational Eagle/Extracted Features/Normalised to r/" + str(encoding_dim) + "_feature_300_epoch_features_1.npy")[0]
 extracted_features_switch = extracted_features.T
 
 print(extracted_features_switch.shape)
@@ -284,7 +284,7 @@ wrap_labels(ax, 10)
 
 
 
-plt.savefig("Variational Eagle/Correlation Plots/individually_normalised_" + str(encoding_dim) + "_feature_pca_9_all_property_correlation_1_abs", bbox_inches='tight')
+plt.savefig("Variational Eagle/Correlation Plots/r_normalised_" + str(encoding_dim) + "_feature_all_property_correlation_1_abs", bbox_inches='tight')
 plt.show()
 
 
@@ -333,7 +333,7 @@ for i, property in enumerate(properties):
         axs[feature][i].set_ylabel(None)
         axs[feature][i].tick_params(labelsize=12)
 
-plt.savefig("Variational Eagle/Correlation Plots/scatter_individually_normalised" + str(encoding_dim) + "_feature_all_property_correlation_1", bbox_inches='tight')
+plt.savefig("Variational Eagle/Correlation Plots/scatter_r_normalised" + str(encoding_dim) + "_feature_all_property_correlation_1", bbox_inches='tight')
 # plt.savefig("Variational Eagle/Correlation Plots/scatter_" + str(encoding_dim) + "_feature_all_property_correlation_p2", bbox_inches='tight')
 plt.show()
 
