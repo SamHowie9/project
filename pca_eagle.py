46da77b3916d062c18801ec5f95dfcb77baf0337
Sam <samhowie9@gmail.com>
2024-11-26 17:27:07 +0000

    Code

--- pca_eagle.py
+++ pca_eagle.py
@@ -45,8 +45,8 @@ for i, galaxy in enumerate(chosen_galaxies):
 
 
     # normalise the image (either each band independently or to the r band)
-    # image = normalise_independently(image)
-    image = normalise_to_r(image)
+    image = normalise_independently(image)
+    # image = normalise_to_r(image)
 
     # add the image to the dataset
     all_images.append(image)
@@ -63,10 +63,14 @@ test_images = np.array(all_images[-200:])
 
 print(train_images.shape)
 
+# flatten the images by reshaping the array from (3424, 256, 256, 3) to (3434, 256 * 256 * 3) = (3424, 19666608)
+flattened_images = train_images.reshape(3424, 196608)
 
 
-# pca = PCA(n_components=30).fit(train_images)
-#
-# plt.plot(range(1, encoding_dim+1), pca.explained_variance_ratio_)
-#
-# plt.show()
+
+pca = PCA(n_components=30).fit(flattened_images)
+
+plt.plot(range(1, 31), pca.explained_variance_ratio_)
+
+plt.savefig("Plots/pca_scree")
+plt.show()
