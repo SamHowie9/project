a91ff9f9f915f2f02356aa2b82328f97f10bfb20
Sam <samhowie9@gmail.com>
2024-12-03 16:20:42 +0000

    Code

--- sersic_transition.py
+++ sersic_transition.py
@@ -32,38 +32,46 @@ for i, galaxy in enumerate(bad_fit):
 
 
 
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"] < 1]), 4))
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(1, 2, inclusive="left")]), 4))
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(2, 3, inclusive="left")]), 4))
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(3, 4, inclusive="left")]), 4))
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(4, 5, inclusive="left")]), 4))
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(5, 6, inclusive="left")]), 4))
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(6, 7, inclusive="left")]), 4))
-# print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"] >= 7]), 4))
+print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"] < 1]), 9))
+print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(1, 2, inclusive="left")]), 9))
+print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(2, 3, inclusive="left")]), 9))
+print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(3, 4, inclusive="left")]), 9))
+print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(4, 5, inclusive="left")]), 9))
+print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(5, 6, inclusive="left")]), 9))
+print(random.sample(list(all_properties["GalaxyID"].loc[all_properties["n_r"] >= 6]), 9))
 
 
-sersic_0_1 = [2599936, 17944813, 8494197, 8485275]
-sersic_1_2 = [16027782, 9489651, 8837359, 9098658]
-sersic_2_3 = [14829299, 9214339, 9971165, 10696181]
-sersic_3_4 = [9292390, 9192917, 14892360, 14111615]
-sersic_4_5 = [9019265, 18002123, 10056399, 9052812]
-sersic_5_6 = [8065154, 12575320, 16701964, 17672887]
-sersic_6_7 = [9526568, 16204628, 14105654, 14747624]
-sersic_7_8 = [14715402, 16657911, 15250310, 15274006]
+# sersic_0_1 = [2599936, 17944813, 8494197, 8485275]
+# sersic_1_2 = [16027782, 9489651, 8837359, 9098658]
+# sersic_2_3 = [14829299, 9214339, 9971165, 10696181]
+# sersic_3_4 = [9292390, 9192917, 14892360, 14111615]
+# sersic_4_5 = [9019265, 18002123, 10056399, 9052812]
+# sersic_5_6 = [8065154, 12575320, 16701964, 17672887]
+# sersic_6_7 = [9526568, 16204628, 14105654, 14747624]
+# sersic_7_8 = [14715402, 16657911, 15250310, 15274006]
+
+sersic_0_1 = [13851369, 9034192, 14100103, 8425845, 65696, 17287247, 12659357, 15885743, 10361791]
+sersic_1_2 = [13160767, 9741348, 10891574, 9712821, 8826313, 8506884, 10006224, 16561331, 17746163]
+sersic_2_3 = [9463308, 13172065, 18174422, 4522287, 2491995, 9282800, 17845684, 14227200, 10672055]
+sersic_3_4 = [9406027, 14895219, 13861189, 9067175, 9106483, 15239966, 9427537, 10140532, 16895359]
+sersic_4_5 = [16614950, 9991035, 9052812, 15289521, 17668707, 9019265, 18253969, 13965937, 16573399]
+sersic_5_6 = [9554090, 16673538, 16018178, 9446777, 13825637, 14143266, 11419698, 17672887, 14916080]
+sersic_6_8 = [12648880, 14570467, 16204628, 15274006, 8266167, 16150066, 15953506, 14715402, 16462661]
+
 
 
 fig = plt.figure(constrained_layout=False, figsize=(15, 12))
 
 # create sub figures within main figure, specify their location
-gs1 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.025, right=0.225, top=0.975, bottom=0.775)
-gs2 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.275, right=0.475, top=0.975, bottom=0.775)
-gs3 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.525, right=0.725, top=0.975, bottom=0.775)
-gs4 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.775, right=0.975, top=0.975, bottom=0.775)
+gs1 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.025, right=0.225, top=0.975, bottom=0.775)
+gs2 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.275, right=0.475, top=0.975, bottom=0.775)
+gs3 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.525, right=0.725, top=0.975, bottom=0.775)
+gs4 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.775, right=0.975, top=0.975, bottom=0.775)
 
-gs5 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.025, right=0.225, top=0.725, bottom=0.525)
-gs6 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.275, right=0.475, top=0.725, bottom=0.525)
-gs7 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.525, right=0.725, top=0.725, bottom=0.525)
-gs8 = fig.add_gridspec(nrows=2, ncols=2, wspace=0.05, hspace=0.05, left=0.775, right=0.975, top=0.725, bottom=0.525)
+gs5 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.025, right=0.225, top=0.725, bottom=0.525)
+gs6 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.275, right=0.475, top=0.725, bottom=0.525)
+gs7 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.525, right=0.725, top=0.725, bottom=0.525)
+gs8 = fig.add_gridspec(nrows=3, ncols=3, wspace=0.05, hspace=0.05, left=0.775, right=0.975, top=0.725, bottom=0.525)
 
 count = 0
 
@@ -113,8 +121,8 @@ for i in range(0, 2):
         g7_ax.get_yaxis().set_visible(False)
 
         g8_ax = fig.add_subplot(gs8[i, j])
-        image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(sersic_7_8[count]) + ".png")
-        g8_ax.imshow(image)
+        # image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(sersic_7_8[count]) + ".png")
+        # g8_ax.imshow(image)
         g8_ax.get_xaxis().set_visible(False)
         g8_ax.get_yaxis().set_visible(False)
 
@@ -148,14 +156,14 @@ ax6.set_title("$6 \leq n < 6$", fontsize=25)
 
 ax7 = fig.add_subplot(gs7[:])
 ax7.axis("off")
-ax7.set_title("$6 \leq n < 7$", fontsize=25)
+ax7.set_title("$n \geq 6", fontsize=25)
 
-ax8 = fig.add_subplot(gs8[:])
-ax8.axis("off")
-ax8.set_title("$n \geq 7$", fontsize=25)
+# ax8 = fig.add_subplot(gs8[:])
+# ax8.axis("off")
+# ax8.set_title("$n \geq 7$", fontsize=25)
 
 
-plt.savefig("Variational Eagle/Plots/sersic_transition_plot", bbox_inches='tight')
+plt.savefig("Variational Eagle/Plots/sersic_transition_plot")
 plt.show()
 
 
