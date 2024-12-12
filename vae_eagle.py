bcf9aa414dd9ae5cd902dfc4c5e2b94415c22043
Sam <samhowie9@gmail.com>
2024-12-12 15:08:49 +0000

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -19,11 +19,11 @@ from matplotlib import image as mpimg
 
 encoding_dim = 25
 
-run = 3
+run = 2
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="9"
+os.environ["CUDA_VISIBLE_DEVICES"]="8"
 
 # number of epochs for run
 epochs = 300
