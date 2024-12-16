28572255b3f79c3332f4d1597ae2447699a1d5af
Sam <samhowie9@gmail.com>
2024-12-16 14:12:40 +0000

    Code

--- balance_dataset.py
+++ balance_dataset.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+# from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 import random
@@ -43,76 +45,100 @@ unknown = list(all_properties["GalaxyID"].loc[all_properties["n_r"].between(2.5,
 ellipticals = list(all_properties["GalaxyID"].loc[all_properties["n_r"] >= 4])
 
 
+print(len(spirals))
+print(len(unknown))
+print(len(ellipticals))
 
-# randomly sample half the spirals (seed for reproducibility)
-random.seed(1)
-spirals = random.sample(spirals, round(len(spirals)/2))
+print(ellipticals)
 
-fig, axs = plt.subplots(4, 4)
+ellipticals = [140114]
 
-all_images = []
+datagen = ImageDataGenerator(rotation_range=360, horizontal_flip=True, vertical_flip=True)
 
-# open and add the spiral galaxies to the dataset
-for galaxy in spirals:
-    image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
-    all_images.append(normalise_independently(image))
 
-# open and add the 'unknown' galaxies to the dataset (sersic index between 2.5 and 4)
-for galaxy in unknown:
-    image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
-    all_images.append(normalise_independently(image))
 
-# open and add all the elliptical galaxies to the dataset
-for n, galaxy in enumerate(ellipticals):
+for galaxy in ellipticals:
 
     image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
-    all_images.append(normalise_independently(image))
 
-    # add three more variants of this image
-
-    # rotate the image by 90 degrees and add random noise
-    image_rot_90 = np.rot90(np.copy(image), k=1)
-    for i in range(0, 3):
-        gaussian = np.random.normal(0, 0.01, (len(image_rot_90[0]), len(image_rot_90[0])))
-        image_rot_90.T[i] = image_rot_90.T[i] + gaussian
-
-    # flip the original image horizontally and add random noise
-    image_flip = np.fliplr(np.copy(image))
-    for i in range(0, 3):
-        gaussian = np.random.normal(0, 0.01, (len(image_flip[0]), len(image_flip[0])))
-        image_flip.T[i] = image_flip.T[i] + gaussian
-
-    # flip the rotated image horizontally and add random noise
-    image_flip_90 = np.fliplr(np.copy(image_rot_90))
-    for i in range(0, 3):
-        gaussian = np.random.normal(0, 0.01, (len(image_flip_90[0]), len(image_flip_90[0])))
-        image_flip_90.T[i] = image_flip_90.T[i] + gaussian
-
-    # add the three variants to the dataset
-    all_images.append(normalise_independently(image_rot_90))
-    all_images.append(normalise_independently(image_flip))
-    all_images.append(normalise_independently(image_flip_90))
+    i = 0
+    for batch in datagen.flow(image, batch_size=1, save_to_dir="/cosma7/data/durham/dc-howi1/project/Eagle Augmented/", save_prefix=galaxy, save_format="png"):
+        i += 1
+        if i > 30:
+            break
 
 
 
-chosen = [1852, 1856, 1860, 1864]
 
-for i, galaxy in enumerate(chosen):
-
-    axs[i, 0].imshow(all_images[galaxy])
-    axs[i, 1].imshow(all_images[galaxy + 1])
-    axs[i, 2].imshow(all_images[galaxy + 2])
-    axs[i, 3].imshow(all_images[galaxy + 3])
-
-
-
-# randomly (seed for reproducibility) rearrange the galaxies
-# random.seed(5)
-# random.shuffle(all_images)
-
-# image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(spirals[0]) + ".png")
-# axs[0, 0].imshow(image)
-# axs[1, 0].imshow(all_images[0])
-
-plt.savefig("Variational Eagle/Plots/elliptical_variations_not_normalised")
-plt.show()
+# # randomly sample half the spirals (seed for reproducibility)
+# random.seed(1)
+# spirals = random.sample(spirals, round(len(spirals)/2))
+#
+# fig, axs = plt.subplots(4, 4)
+#
+# all_images = []
+#
+# # open and add the spiral galaxies to the dataset
+# for galaxy in spirals:
+#     image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
+#     all_images.append(normalise_independently(image))
+#
+# # open and add the 'unknown' galaxies to the dataset (sersic index between 2.5 and 4)
+# for galaxy in unknown:
+#     image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
+#     all_images.append(normalise_independently(image))
+#
+# # open and add all the elliptical galaxies to the dataset
+# for n, galaxy in enumerate(ellipticals):
+#
+#     image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
+#     all_images.append(normalise_independently(image))
+#
+#     # add three more variants of this image
+#
+#     # rotate the image by 90 degrees and add random noise
+#     image_rot_90 = np.rot90(np.copy(image), k=1)
+#     for i in range(0, 3):
+#         gaussian = np.random.normal(0, 0.01, (len(image_rot_90[0]), len(image_rot_90[0])))
+#         image_rot_90.T[i] = image_rot_90.T[i] + gaussian
+#
+#     # flip the original image horizontally and add random noise
+#     image_flip = np.fliplr(np.copy(image))
+#     for i in range(0, 3):
+#         gaussian = np.random.normal(0, 0.01, (len(image_flip[0]), len(image_flip[0])))
+#         image_flip.T[i] = image_flip.T[i] + gaussian
+#
+#     # flip the rotated image horizontally and add random noise
+#     image_flip_90 = np.fliplr(np.copy(image_rot_90))
+#     for i in range(0, 3):
+#         gaussian = np.random.normal(0, 0.01, (len(image_flip_90[0]), len(image_flip_90[0])))
+#         image_flip_90.T[i] = image_flip_90.T[i] + gaussian
+#
+#     # add the three variants to the dataset
+#     all_images.append(normalise_independently(image_rot_90))
+#     all_images.append(normalise_independently(image_flip))
+#     all_images.append(normalise_independently(image_flip_90))
+#
+#
+#
+# chosen = [1852, 1856, 1860, 1864]
+#
+# for i, galaxy in enumerate(chosen):
+#
+#     axs[i, 0].imshow(all_images[galaxy])
+#     axs[i, 1].imshow(all_images[galaxy + 1])
+#     axs[i, 2].imshow(all_images[galaxy + 2])
+#     axs[i, 3].imshow(all_images[galaxy + 3])
+#
+#
+#
+# # randomly (seed for reproducibility) rearrange the galaxies
+# # random.seed(5)
+# # random.shuffle(all_images)
+#
+# # image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(spirals[0]) + ".png")
+# # axs[0, 0].imshow(image)
+# # axs[1, 0].imshow(all_images[0])
+#
+# plt.savefig("Variational Eagle/Plots/elliptical_variations_not_normalised")
+# plt.show()
