bde3133ba9d46876fd1f951f5fe084dca6961616
Sam <samhowie9@gmail.com>
2025-01-01 16:17:25 +0000

    Code

--- test.py
+++ test.py
@@ -4,41 +4,72 @@ from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 import random
 # import cv2
+import keras
 
 
-A = [[1, 2], [3, 4], [5, 6], [7, 8]]
-B = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-B = [[3, 3], [5, 5], [9, 9], [1, 1], [1, 1], [1, 1], [1, 1], [2, 2], [2, 2], [2, 2], [2, 2], [3, 3], [3, 3], [3, 3], [3, 3], [4, 4], [4, 4], [4, 4], [4, 4]]
-
-
-
-chosen_galaxies = np.load("Galaxy Properties/Eagle Properties/Chosen Galaxies.npy")
-
-print(len(chosen_galaxies))
+# A = [[1, 2], [3, 4], [5, 6], [7, 8]]
+# B = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# B = [[3, 3], [5, 5], [9, 9], [1, 1], [1, 1], [1, 1], [1, 1], [2, 2], [2, 2], [2, 2], [2, 2], [3, 3], [3, 3], [3, 3], [3, 3], [4, 4], [4, 4], [4, 4], [4, 4]]
 
 
-# load structural and physical properties into dataframes
-structure_properties = pd.read_csv("Galaxy Properties/Eagle Properties/structure_propeties.csv", comment="#")
-physical_properties = pd.read_csv("Galaxy Properties/Eagle Properties/physical_properties.csv", comment="#")
 
-# # account for hte validation data and remove final 200 elements
-# structure_properties.drop(structure_properties.tail(200).index, inplace=True)
-# physical_properties.drop(physical_properties.tail(200).index, inplace=True)
+# chosen_galaxies = np.load("Galaxy Properties/Eagle Properties/Chosen Galaxies.npy")
+#
+# print(len(chosen_galaxies))
+#
+#
+# # load structural and physical properties into dataframes
+# structure_properties = pd.read_csv("Galaxy Properties/Eagle Properties/structure_propeties.csv", comment="#")
+# physical_properties = pd.read_csv("Galaxy Properties/Eagle Properties/physical_properties.csv", comment="#")
+#
+# # # account for hte validation data and remove final 200 elements
+# # structure_properties.drop(structure_properties.tail(200).index, inplace=True)
+# # physical_properties.drop(physical_properties.tail(200).index, inplace=True)
+#
+# # dataframe for all properties
+# all_properties = pd.merge(structure_properties, physical_properties, on="GalaxyID")
+#
+# print(len(all_properties))
+#
+# # find all bad fit galaxies
+# bad_fit = all_properties[((all_properties["flag_r"] == 4) | (all_properties["flag_r"] == 1) | (all_properties["flag_r"] == 5))].index.tolist()
+# # print(bad_fit)
+#
+# # remove those galaxies
+# for galaxy in bad_fit:
+#     all_properties = all_properties.drop(galaxy, axis=0)
+#
+# print(len(list(all_properties["GalaxyID"])))
+#
+# print(len(all_properties))
+#
+# print(list(all_properties["GalaxyID"]))
 
-# dataframe for all properties
-all_properties = pd.merge(structure_properties, physical_properties, on="GalaxyID")
 
-print(len(all_properties))
 
-# find all bad fit galaxies
-bad_fit = all_properties[((all_properties["flag_r"] == 4) | (all_properties["flag_r"] == 1) | (all_properties["flag_r"] == 5))].index.tolist()
-# print(bad_fit)
+# normalise each band individually
+def normalise_independently(image):
+    image = image.T
+    for i in range(0, 3):
+        image[i] = (image[i] - np.min(image[i])) / (np.max(image[i]) - np.min(image[i]))
+    return image.T
 
-# remove those galaxies
-for galaxy in bad_fit:
-    all_properties = all_properties.drop(galaxy, axis=0)
 
-print(len(list(all_properties["GalaxyID"])))
+data_ids = [2, 13632, 21794, 23302, 24478]
+reconstruction_ids = [26264, 27474, 28851, 29818, 30903]
 
-print(len(all_properties))
+data = []
+reconstruction = []
+
+for galaxy in data_ids:
+    image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
+    data.append(normalise_independently(image))
+
+for galaxy in reconstruction_ids:
+    image = mpimg.imread("/cosma7/data/Eagle/web-storage/RefL0100N1504_Subhalo/galrand_" + str(galaxy) + ".png")
+    reconstruction.append(normalise_independently(image))
+
+
+loss = keras.losses.binary_crossentropy(data, reconstruction)
+print(loss)
