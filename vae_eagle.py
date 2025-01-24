b09ac854d4a57f355a0a15e294eae940fa0cf026
Sam <samhowie9@gmail.com>
2025-01-24 16:52:43 +0000

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -20,7 +20,7 @@ run = 1
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="6"
+os.environ["CUDA_VISIBLE_DEVICES"]="7"
 
 # number of epochs for run
 epochs = 750
