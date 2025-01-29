07ce90e766e6dccda1c381a3f845322160260c4d
Sam <samhowie9@gmail.com>
2025-01-29 11:31:52 +0000

    Code

--- galaxy_zoo.py
+++ galaxy_zoo.py
@@ -1,4 +1,15 @@
 import zipfile
+from matplotlib import image as mpimg
 
-with zipfile.ZipFile("gz2_images_all.zip", "r") as zip_ref:
-    zip_ref.extractall("Galaxy Zoo Images")
+
+# with zipfile.ZipFile("gz2_images_all.zip", "r") as zip_ref:
+#     zip_ref.extractall("Galaxy Zoo Images")
+
+
+
+
+
+
+image = mpimg.imread("/cosma7/data/durham/dc-howi1/project/project/Galaxy Zoo Images/gz2_images_all/587739379910508635.jpg")
+
+print(image.shape)
