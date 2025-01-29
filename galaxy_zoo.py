4c3ea722dffd5d1268680b321a30c4f8254bd803
Sam <samhowie9@gmail.com>
2025-01-29 11:34:25 +0000

    Code

--- galaxy_zoo.py
+++ galaxy_zoo.py
@@ -8,12 +8,19 @@ import numpy as np
 
 
 
-
+# normalise each band individually
+def normalise_independently(image):
+    image = image.T
+    for i in range(0, 3):
+        image[i] = (image[i] - np.min(image[i])) / (np.max(image[i]) - np.min(image[i]))
+    return image.T
 
 
 image = mpimg.imread("/cosma7/data/durham/dc-howi1/project/project/Galaxy Zoo Images/gz2_images_all/587739379910508635.jpg")
 
 smallest_non_zero = np.min(image[image > 0])
 image = np.where(image == 0.0, smallest_non_zero, image)
+
+image = normalise_independently(image)
 
 print(image.shape)
