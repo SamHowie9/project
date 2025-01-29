8adbdd763cbea6c7c69ce59f26761e7eed404721
Sam <samhowie9@gmail.com>
2025-01-29 11:32:58 +0000

    Code

--- galaxy_zoo.py
+++ galaxy_zoo.py
@@ -1,5 +1,6 @@
 import zipfile
 from matplotlib import image as mpimg
+import numpy as np
 
 
 # with zipfile.ZipFile("gz2_images_all.zip", "r") as zip_ref:
@@ -11,5 +12,8 @@ from matplotlib import image as mpimg
 
 
 image = mpimg.imread("/cosma7/data/durham/dc-howi1/project/project/Galaxy Zoo Images/gz2_images_all/587739379910508635.jpg")
+
+smallest_non_zero = np.min(image[image > 0])
+image = np.where(image == 0.0, smallest_non_zero, image)
 
 print(image.shape)
