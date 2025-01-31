f5caf4e9d3ac0826a6a9c0c87e6d8478e66af01b
Sam <samhowie9@gmail.com>
2025-01-31 11:39:36 +0000

    Code

--- galaxy_zoo.py
+++ galaxy_zoo.py
@@ -46,6 +46,7 @@ for i, galaxy in enumerate(galaxies):
     #     # shrink the image to 256x256 using area interpolation (best for shrinking images)
     #     image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
 
+    print(image.shape)
 
     # add the image to the dataset
     all_images.append(image)
