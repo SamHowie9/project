6dd8798f8acf86985aae3219358cd9ccf9b19475
Sam <samhowie9@gmail.com>
2025-01-31 16:16:58 +0000

    Code

--- vae_zoo.py
+++ vae_zoo.py
@@ -16,12 +16,12 @@ import cv2
 
 
 
-encoding_dim = 40
-run = 3
+encoding_dim = 30
+run = 1
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="4"
 
 # number of epochs for run
 epochs = 750
