223e6549c034a1baec2d5c22e49394bb8332ee20
Sam <samhowie9@gmail.com>
2025-01-31 11:05:12 +0000

    Code

--- vae_zoo.py
+++ vae_zoo.py
@@ -24,7 +24,7 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 # number of epochs for run
-epochs = 500
+epochs = 750
 
 # batch size for run
 batch_size = 32
