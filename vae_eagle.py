4949c853bd1265d44bbd1416eeeca4c590c84629
Sam <samhowie9@gmail.com>
2025-02-18 17:18:08 +0000

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -250,6 +250,9 @@ for run in range(1, 4):
     augmented_galaxies = os.listdir("/cosma7/data/durham/dc-howi1/project/Eagle Augmented/Unknown/")
 
     for galaxy in augmented_galaxies:
+
+        print(galaxy)
+
         # load each augmented image
         image = mpimg.imread("/cosma7/data/durham/dc-howi1/project/Eagle Augmented/Unknown/" + galaxy)
 
