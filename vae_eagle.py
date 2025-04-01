400e9e6fe32fc7e563e529abd4ffad20e3f01f72
Sam <samhowie9@gmail.com>
2025-04-01 17:55:11 +0100

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -16,11 +16,11 @@ from matplotlib import image as mpimg
 
 
 encoding_dim = 25
-run = 2
+run = 3
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 # number of epochs for run
 epochs = 200
@@ -33,7 +33,7 @@ batch_size = 32
 
 
 # for run in range(2, 4):
-for run in [2]:
+for run in [3]:
 
     # normalise each band individually
     def normalise_independently(image):
