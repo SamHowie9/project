82e419078ea0991e22755fc86945c76e9fe4a383
Sam <samhowie9@gmail.com>
2025-04-09 15:15:15 +0100

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -20,7 +20,7 @@ run = 1
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="6"
+os.environ["CUDA_VISIBLE_DEVICES"]="7"
 
 # number of epochs for run
 epochs = 750
@@ -443,8 +443,9 @@ for run in [1]:
                 # print("Reconstruction Loss Shape:", reconstruction_loss.shape)
                 # reconstruction_loss = ops.sqrt(ops.mean(ops.square(data - reconstruction), axis=(1, 2, 3)))
                 # print("Reconstruction Loss Shape:", reconstruction_loss.shape)
-                reconstruction_loss = ops.mean(ops.sqrt(ops.mean(ops.square(data - reconstruction), axis=(1, 2, 3))))
+                # reconstruction_loss = ops.mean(ops.sqrt(ops.mean(ops.square(data - reconstruction), axis=(1, 2, 3))))
                 # print("Reconstruction Loss Shape:", reconstruction_loss.shape)
+                reconstruction_loss = ops.mean(ops.sum(ops.sqrt(ops.mean(ops.square(data-reconstruction), axis=3)), axis=(1, 2)))
 
 
 
@@ -595,13 +596,13 @@ for run in [1]:
 
     # save the weights
     # vae.save_weights(filepath="Variational Eagle/Weights/Fully Balanced Mean/" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_weights_" + str(run) + ".weights.h5", overwrite=True)
-    vae.save_weights(filepath="Variational Eagle/Weights/Test/rmse_mean_sum.weights.h5", overwrite=True)
+    vae.save_weights(filepath="Variational Eagle/Weights/Test/rmse_sum_sum.weights.h5", overwrite=True)
 
 
     # generate extracted features from trained encoder and save as numpy array
     extracted_features = vae.encoder.predict(train_images)
     # np.save("Variational Eagle/Extracted Features/Fully Balanced Mean/" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_features_" + str(run) + ".npy", extracted_features)
-    np.save("Variational Eagle/Extracted Features/Test/rmse_mean_sum.npy", extracted_features)
+    np.save("Variational Eagle/Extracted Features/Test/rmse_sum_sum.npy", extracted_features)
 
     print(np.array(extracted_features).shape)
 
@@ -610,7 +611,7 @@ for run in [1]:
     print("\n \n" + str(encoding_dim))
     print(str(loss[0]) + "   " + str(loss[1]) + "   " + str(loss[2]) + "\n")
     # np.save("Variational Eagle/Loss/Fully Balanced Mean/" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_loss_" + str(run) + ".npy", loss)
-    np.save("Variational Eagle/Loss/Test/rmse_mean_sum.npy", loss)
+    np.save("Variational Eagle/Loss/Test/rmse_sum_sum.npy", loss)
 
 
 
@@ -675,7 +676,7 @@ for run in [1]:
 
 
     # plt.savefig("Variational Eagle/Loss Plots/fully_balanced_mean_" + str(encoding_dim) + "_feature_" + str(epochs) + "_epochs_" + str(batch_size) + "_bs_loss_" + str(run))
-    plt.savefig("Variational Eagle/Loss Plots/test_rmse_mean_sum")
+    plt.savefig("Variational Eagle/Loss Plots/test_rmse_sum_sum")
     plt.show()
 
 
@@ -755,7 +756,7 @@ for run in [1]:
         axs[1, i].get_yaxis().set_visible(False)
 
     # plt.savefig("Variational Eagle/Reconstructions/Training/fully_balanced_mean_" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_reconstruction_" + str(run))
-    plt.savefig("Variational Eagle/Reconstructions/Training/test_rmse_mean_sum")
+    plt.savefig("Variational Eagle/Reconstructions/Training/test_rmse_sum_sum")
     plt.show()
 
 
@@ -797,7 +798,7 @@ for run in [1]:
         axs[1,i].get_yaxis().set_visible(False)
 
     # plt.savefig("Variational Eagle/Reconstructions/Testing/fully_balanced_mean_" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_reconstruction_" + str(run))
-    plt.savefig("Variational Eagle/Reconstructions/Testing/test_rmse_mean_sum")
+    plt.savefig("Variational Eagle/Reconstructions/Testing/test_rmse_sum_sum")
     plt.show()
 
 
