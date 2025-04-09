38761bff4dc889a5b83e6624e1c3b5f493178b0a
Sam <samhowie9@gmail.com>
2025-04-09 17:20:46 +0100

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -20,7 +20,7 @@ run = 1
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="7"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 # number of epochs for run
 epochs = 750
@@ -445,6 +445,7 @@ for run in [1]:
                 # print("Reconstruction Loss Shape:", reconstruction_loss.shape)
                 # reconstruction_loss = ops.mean(ops.sqrt(ops.mean(ops.square(data - reconstruction), axis=(1, 2, 3))))
                 # print("Reconstruction Loss Shape:", reconstruction_loss.shape)
+
                 # reconstruction_loss = ops.mean(ops.sum(ops.sqrt(ops.mean(ops.square(data-reconstruction), axis=3)), axis=(1, 2)))
                 # reconstruction_loss = reconstruction_loss * 256 * 256
 
@@ -493,7 +494,7 @@ for run in [1]:
 
                 # total loss is the sum of reconstruction loss and kl divergence
                 # total_loss = reconstruction_loss + kl_loss
-                total_loss = reconstruction_loss + (1000 * kl_loss)
+                total_loss = reconstruction_loss + (256 * 256 * kl_loss)
 
                 print("Total Loss Shape:", total_loss.shape)
 
@@ -608,13 +609,13 @@ for run in [1]:
 
     # save the weights
     # vae.save_weights(filepath="Variational Eagle/Weights/Fully Balanced Mean/" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_weights_" + str(run) + ".weights.h5", overwrite=True)
-    vae.save_weights(filepath="Variational Eagle/Weights/Test/bce_beta_1000.weights.h5", overwrite=True)
+    vae.save_weights(filepath="Variational Eagle/Weights/Test/bce_beta_256.weights.h5", overwrite=True)
 
 
     # generate extracted features from trained encoder and save as numpy array
     extracted_features = vae.encoder.predict(train_images)
     # np.save("Variational Eagle/Extracted Features/Fully Balanced Mean/" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_features_" + str(run) + ".npy", extracted_features)
-    np.save("Variational Eagle/Extracted Features/Test/bce_beta_1000.npy", extracted_features)
+    np.save("Variational Eagle/Extracted Features/Test/bce_beta_256.npy", extracted_features)
 
     print(np.array(extracted_features).shape)
 
@@ -623,7 +624,7 @@ for run in [1]:
     print("\n \n" + str(encoding_dim))
     print(str(loss[0]) + "   " + str(loss[1]) + "   " + str(loss[2]) + "\n")
     # np.save("Variational Eagle/Loss/Fully Balanced Mean/" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_loss_" + str(run) + ".npy", loss)
-    np.save("Variational Eagle/Loss/Test/bce_beta_1000.npy", loss)
+    np.save("Variational Eagle/Loss/Test/bce_beta_256.npy", loss)
 
 
 
@@ -688,7 +689,7 @@ for run in [1]:
 
 
     # plt.savefig("Variational Eagle/Loss Plots/fully_balanced_mean_" + str(encoding_dim) + "_feature_" + str(epochs) + "_epochs_" + str(batch_size) + "_bs_loss_" + str(run))
-    plt.savefig("Variational Eagle/Loss Plots/test_bce_beta_1000")
+    plt.savefig("Variational Eagle/Loss Plots/test_bce_beta_256")
     plt.show()
 
 
@@ -768,7 +769,7 @@ for run in [1]:
         axs[1, i].get_yaxis().set_visible(False)
 
     # plt.savefig("Variational Eagle/Reconstructions/Training/fully_balanced_mean_" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_reconstruction_" + str(run))
-    plt.savefig("Variational Eagle/Reconstructions/Training/test_bce_beta_1000")
+    plt.savefig("Variational Eagle/Reconstructions/Training/test_bce_beta_256")
     plt.show()
 
 
@@ -810,7 +811,7 @@ for run in [1]:
         axs[1,i].get_yaxis().set_visible(False)
 
     # plt.savefig("Variational Eagle/Reconstructions/Testing/fully_balanced_mean_" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_reconstruction_" + str(run))
-    plt.savefig("Variational Eagle/Reconstructions/Testing/test_bce_beta_1000")
+    plt.savefig("Variational Eagle/Reconstructions/Testing/test_bce_beta_256")
     plt.show()
 
 
