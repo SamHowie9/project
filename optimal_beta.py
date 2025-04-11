b7b97ff06c81da7c1c36493c9e1c6ad786a0e9d3
Sam <samhowie9@gmail.com>
2025-04-11 16:56:41 +0100

    Code

--- optimal_beta.py
+++ optimal_beta.py
@@ -16,9 +16,33 @@ import random
 
 betas = [0.001, 0.001, 0.00001, 0.000001, 0.0000001]
 
-reconstruction_loss = np.load("Variational Eagle/Loss/test")
+total_loss = np.load("Variational Eagle/Loss/Test/total_loss_beta.npy")
+reconstruction_loss = np.load("Variational Eagle/Loss/Test/reconstruction_loss_beta.npy")
+kl_loss = np.load("Variational Eagle/Loss/Test/kl_loss_beta.npy")
 
 fig, axs = plt.subplots(3, 1, figsize=(12, 15))
+
+# fig, axs = plt.subplots(1, 1, figsize=(12, 5))
+
+
+beta_words = ["0.001", "0.0001", "0.00001", "0.000001", "0.0000001"]
+
+# axs.plot(y=reconstruction_loss, x=beta_words)
+axs[0].plot(beta_words, total_loss)
+axs[0].scatter(beta_words, total_loss)
+axs[0].set_title("Total Loss")
+
+axs[1].plot(beta_words, reconstruction_loss)
+axs[1].scatter(beta_words, reconstruction_loss)
+axs[1].set_title("Reconstruction Loss")
+
+axs[2].plot(beta_words, kl_loss)
+axs[2].scatter(beta_words, kl_loss)
+axs[2].set_title("KL Loss")
+
+# plt.legend()
+plt.savefig("Variational Eagle/Plots/optimal_beta", bbox_inches='tight')
+plt.show()
 
 
 
