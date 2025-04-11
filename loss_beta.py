20b595a813118848071cfb32dcda9ce736b8a99f
Sam <samhowie9@gmail.com>
2025-04-11 11:43:49 +0100

    Code

--- loss_beta.py
+++ loss_beta.py
@@ -18,8 +18,8 @@ encoding_dim = 25
 run = 1
 
 # select which gpu to use
-# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 # number of epochs for run
 epochs = 750
