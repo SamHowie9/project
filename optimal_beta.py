8e7ee4a869f9001f29c02faf7767294adeadd300
Sam <samhowie9@gmail.com>
2025-04-14 13:05:07 +0100

    Code

--- optimal_beta.py
+++ optimal_beta.py
@@ -14,7 +14,7 @@ import random
 
 # for run_number, (beta, filename) in enumerate([[0.001, "001"], [0.0001, "0001"], [0.00001, "00001"], [0.000001, "000001"], [0.0000001, "0000001"]]):
 
-betas = [0.001, 0.001, 0.00001, 0.000001, 0.0000001]
+betas = [0.01, 0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001, 0.0000005, 0.0000001, 0.00000005, 0.00000001]
 
 total_loss = np.load("Variational Eagle/Loss/Test/total_loss_beta.npy")
 reconstruction_loss = np.load("Variational Eagle/Loss/Test/reconstruction_loss_beta.npy")
@@ -25,20 +25,31 @@ fig, axs = plt.subplots(3, 1, figsize=(12, 15))
 # fig, axs = plt.subplots(1, 1, figsize=(12, 5))
 
 
-beta_words = ["0.001", "0.0001", "0.00001", "0.000001", "0.0000001"]
+# beta_words = ["0.001", "0.0001", "0.00001", "0.000001", "0.0000001"]
+beta_scale = [1, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7]
+# beta_words = ["1e-2", "1e-3", "5e-4", "1e-4", "5e-5", "1e-5", "5e-6", "1e-6", "5e-7", "1e-7", "5e-8", "1e-8"]
+beta_words = ["1e-2", "1e-3", "1e-4", "1e-5", "1e-6", "1e-7", "1e-8"]
 
 # axs.plot(y=reconstruction_loss, x=beta_words)
-axs[0].plot(beta_words, total_loss)
-axs[0].scatter(beta_words, total_loss)
+axs[0].plot(beta_scale, total_loss)
+axs[0].scatter(beta_scale, total_loss)
 axs[0].set_title("Total Loss")
+axs[0].set_xticks([1, 2, 3, 4, 5, 6, 7])
+axs[0].set_xticklabels(beta_words)
 
-axs[1].plot(beta_words, reconstruction_loss)
-axs[1].scatter(beta_words, reconstruction_loss)
+axs[1].plot(beta_scale, reconstruction_loss)
+axs[1].scatter(beta_scale, reconstruction_loss)
 axs[1].set_title("Reconstruction Loss")
+axs[1].set_xticks([1, 2, 3, 4, 5, 6, 7])
+axs[1].set_xticklabels(beta_words)
 
-axs[2].plot(beta_words, kl_loss)
-axs[2].scatter(beta_words, kl_loss)
-axs[2].set_title("KL Loss")
+
+axs[2].plot(beta_scale, kl_loss)
+axs[2].scatter(beta_scale, kl_loss)
+axs[2].set_title("KL Divergence")
+axs[2].set_xticks([1, 2, 3, 4, 5, 6, 7])
+axs[2].set_xticklabels(beta_words)
+
 
 # plt.legend()
 plt.savefig("Variational Eagle/Plots/optimal_beta", bbox_inches='tight')
