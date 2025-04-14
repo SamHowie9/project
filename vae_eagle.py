10aabd4e10bc19c22672297851c29b903583f885
Sam <samhowie9@gmail.com>
2025-04-14 18:14:24 +0100

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -20,7 +20,7 @@ run = 1
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="7"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 # number of epochs for run
 epochs = 750
@@ -34,7 +34,7 @@ batch_size = 32
 
 # for run in range(2, 4):
 # for run in [1]:
-for beta, beta_name in [[0.00000001, "00000001"], [0.00000005, "00000005"]]:
+for beta, beta_name in [[0.01, "01"], [0.05, "05"]]:
 
     print("Beta:", beta, beta_name)
 
