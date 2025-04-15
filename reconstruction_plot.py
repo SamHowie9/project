c5927ea131471e49d6aba86a838afed3cdc5a128
Sam <samhowie9@gmail.com>
2025-04-15 12:39:44 +0100

    Code

--- reconstruction_plot.py
+++ reconstruction_plot.py
@@ -355,7 +355,7 @@ for run_number, (beta, filename) in enumerate([[0.01, "01"], [0.001, "001"], [0.
         axs[run_number+1, i].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 
 
-    axs[run_number+1][0].set_ylabel("β = " + str(beta))
+    axs[run_number+1][0].set_ylabel(str(beta))
 
 # plt.savefig("Variational Eagle/Reconstructions/Testing/fully_balanced_mean_" + str(encoding_dim) + "_feature_" + str(epochs) + "_epoch_" + str(batch_size) + "_bs_reconstruction_" + str(run))
 plt.savefig("Variational Eagle/Plots/beta_comparison_reconstruction_2", bbox_inches='tight')
