50b838c973b751a93290b3c2d685bfd9c8edaca8
Sam <samhowie9@gmail.com>
2025-04-24 14:39:22 +0100

    Code

--- vae_eagle.py
+++ vae_eagle.py
@@ -15,14 +15,14 @@ from matplotlib import image as mpimg
 
 
 
-encoding_dim = 20
+encoding_dim = 21
 run = 1
 beta = 0.0001
 beta_name = "0001"
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 # number of epochs for run
 epochs = 300
@@ -38,7 +38,7 @@ batch_size = 32
 # for run in [1]:
 # for beta, beta_name in [[0.001, "001"], [0.0001, "0001"], [0.00001, "00001"], [0.000001, "000001"]]:
 
-for run in [2, 3]:
+for run in [1, 2, 3]:
 
     print()
     print("Encoding Dim", encoding_dim)
