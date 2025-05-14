f1c2c8bacd4d7c8187a0a0391de562c52af2c474
Sam <samhowie9@gmail.com>
2025-05-14 16:27:23 +0100

    Code

--- normalising_vae_eagle.py
+++ normalising_vae_eagle.py
@@ -15,13 +15,13 @@ from matplotlib import image as mpimg
 
 
 encoding_dim = 30
-run = 2
+run = 3
 beta = 0.0001
 beta_name = "0001"
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
 # number of epochs for run
 epochs = 300
