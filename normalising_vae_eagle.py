44cf23dbd13b2b53cdaa6084e8724e4af6c4c3b8
Sam <samhowie9@gmail.com>
2025-05-16 15:06:20 +0100

    Code

--- normalising_vae_eagle.py
+++ normalising_vae_eagle.py
@@ -22,13 +22,13 @@ tfd = tfp.distributions
 
 
 encoding_dim = 30
-run = 2
+run = 3
 beta = 0.0001
 beta_name = "0001"
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 print(tf.config.list_physical_devices('GPU'))
 
