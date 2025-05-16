c6f262cf661a1c1329217fb93c92c783af360a7b
Sam <samhowie9@gmail.com>
2025-05-16 17:15:11 +0100

    Code

--- normalising_vae_eagle.py
+++ normalising_vae_eagle.py
@@ -26,8 +26,8 @@ tfd = tfp.distributions
 encoding_dim = 30
 run = 1
 n_flows = 2
-beta = 0.001
-beta_name = "001"
+beta = 0.0001
+beta_name = "0001"
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
