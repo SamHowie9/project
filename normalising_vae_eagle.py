b64cf2941d67cb84fae602d612ee7f3402082f1f
Sam <samhowie9@gmail.com>
2025-05-19 11:47:05 +0100

    Code

--- normalising_vae_eagle.py
+++ normalising_vae_eagle.py
@@ -26,12 +26,12 @@ tfd = tfp.distributions
 encoding_dim = 30
 run = 1
 n_flows = 2
-beta = 0.1
-beta_name = "1"
+beta = 1
+beta_name = "0"
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="4"
+os.environ["CUDA_VISIBLE_DEVICES"]="5"
 
 
 # number of epochs for run
