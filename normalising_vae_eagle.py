ded4123ba322afffd01f3bbd165780d9eef259c8
Sam <samhowie9@gmail.com>
2025-05-20 11:15:40 +0100

    Code

--- normalising_vae_eagle.py
+++ normalising_vae_eagle.py
@@ -24,14 +24,14 @@ tfd = tfp.distributions
 
 
 encoding_dim = 30
-run = 2
-n_flows = 1
+run = 3
+n_flows = 2
 beta = 0.0001
 beta_name = "0001"
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="6"
+os.environ["CUDA_VISIBLE_DEVICES"]="7"
 
 
 # number of epochs for run
@@ -680,7 +680,7 @@ for encoding_dim in [encoding_dim]:
 
     # build and compile the VAE
     vae = VAE(encoder, decoder)
-    vae.compile(optimizer=optimizers.Adam())
+    vae.compile(optimizer=optimizers.Adam(clipnorm=1.0))
 
 
 
