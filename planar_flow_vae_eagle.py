c5b40dd674bb94e42728fc788dd3fd228ae03a45
Sam <samhowie9@gmail.com>
2025-05-22 14:23:40 +0100

    Code

--- planar_flow_vae_eagle.py
+++ planar_flow_vae_eagle.py
@@ -28,13 +28,13 @@ tfd = tfp.distributions
 
 encoding_dim = 30
 run = 1
-n_flows = 2
+n_flows = 3
 beta = 0.0001
 beta_name = "0001"
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="4"
+os.environ["CUDA_VISIBLE_DEVICES"]="7"
 
 
 # number of epochs for run
