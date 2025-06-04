dc765f01ab6e2e661183963a831c0d10247fa29a
Sam <samhowie9@gmail.com>
2025-06-04 19:23:30 +0100

    Code

--- planar_flow_vae_eagle.py
+++ planar_flow_vae_eagle.py
@@ -24,8 +24,8 @@ tfd = tfp.distributions
 
 
 
-run = 1
-encoding_dim = 35
+run = 3
+encoding_dim = 20
 n_flows = 3
 beta = 0.0001
 beta_name = "0001"
@@ -35,7 +35,7 @@ batch_size = 32
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="4"
+os.environ["CUDA_VISIBLE_DEVICES"]="5"
 
 
 
@@ -45,7 +45,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="4"
 # for encoding_dim in [encoding_dim]:
 # for n_flows in [1, 2, 3]:
 # for encoding_dim, n_flows in [[encoding_dim, 1], [encoding_dim, 2], [encoding_dim, 3], [encoding_dim+1, 1], [encoding_dim+1, 2], [encoding_dim+1, 3]]:
-for encoding_dim in [encoding_dim, encoding_dim+1, encoding_dim+2, encoding_dim+3, encoding_dim+4, encoding_dim+5]:
+for encoding_dim in [encoding_dim, encoding_dim+1, encoding_dim+2, encoding_dim+3, encoding_dim+4]:
 
     print("\n \n")
     print("Encoding Dim", encoding_dim)
