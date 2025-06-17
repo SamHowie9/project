af648883785409f5a1e5ef8507094b7793f602a9
Sam <samhowie9@gmail.com>
2025-06-17 15:34:06 +0100

    Code

--- balance_dataset.py
+++ balance_dataset.py
@@ -191,21 +191,21 @@ print(len(augmented_transitional)+len(unknown))
 print(len(augmented_ellipticals)+len(ellipticals))
 print()
 
-for galaxy in spirals:
-    count = 0
-    for file in augmented_spirals:
-        if file.startswith(str(galaxy)):
-            count += 1
-    if count < 2:
-        print(galaxy, count)
+# for galaxy in spirals:
+#     count = 0
+#     for file in augmented_spirals:
+#         if file.startswith(str(galaxy)):
+#             count += 1
+#     if count < 2:
+#         print(galaxy, count)
 
-for galaxy in transitional:
-    count = 0
-    for file in augmented_transitional:
-        if file.startswith(str(galaxy)):
-            count += 1
-    if count < 26:
-        print(galaxy, count)
+# for galaxy in transitional:
+#     count = 0
+#     for file in augmented_transitional:
+#         if file.startswith(str(galaxy)):
+#             count += 1
+#     if count < 8:
+#         print(galaxy, count)
 
 
 for galaxy in ellipticals:
@@ -213,7 +213,7 @@ for galaxy in ellipticals:
     for file in augmented_ellipticals:
         if file.startswith(str(galaxy)):
             count += 1
-    if count < 20:
+    if count < 8:
         print(galaxy, count)
 
 
