b920d4b4fe9afd376ec585cfdc400b712c6525b0
Sam <samhowie9@gmail.com>
2025-06-26 12:16:00 +0100

    Code

--- planar_flow_vae_eagle.py
+++ planar_flow_vae_eagle.py
@@ -42,7 +42,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 
 
-# for encoding_dim in [encoding_dim]:
+for encoding_dim in [encoding_dim]:
 # for n_flows in [1, 2, 3]:
 # for encoding_dim, n_flows in [[encoding_dim, 1], [encoding_dim, 2], [encoding_dim, 3], [encoding_dim+1, 1], [encoding_dim+1, 2], [encoding_dim+1, 3]]:
 # for encoding_dim in [encoding_dim, encoding_dim+1, encoding_dim+2, encoding_dim+3, encoding_dim+4, encoding_dim+5]:
@@ -50,7 +50,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0"
 # for encoding_dim in range(encoding_dim, encoding_dim+5):
 # for encoding_dim in [49, 50]:
 # for encoding_dim in [encoding_dim]:
-for run in [1, 2, 3]:
+# for run in [1, 2, 3]:
 # for run in range(run, run+14):
 
 
