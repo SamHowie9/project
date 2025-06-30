28e4bfaaa0b1d019b7cc9223b5b2a1b837558e1b
Sam <samhowie9@gmail.com>
2025-06-30 17:23:53 +0100

    Code

--- transition_plot_planar.py
+++ transition_plot_planar.py
@@ -449,9 +449,9 @@ for i, feature in enumerate(chosen_features):
 
     # axs[i][0].set_ylabel(feature+1, rotation=0, labelpad=40, va='center')
 
-axs[0][0].set_ylabel("dCor < 0.2", fonstsize=60, rotation=0, labelpad=15, va='center', ha="right")
-axs[1][0].set_ylabel("Sérsic Index\ndCor = 0.74", fonstsize=60, rotation=0, labelpad=15, va="center", ha="right")
-axs[2][0].set_ylabel("Half-Light Radius\ndCor = 0.81", fonstsize=60, rotation=0, labelpad=15, va="center", ha="right")
+axs[0][0].set_ylabel("dCor < 0.2", fontsize=60, rotation=0, labelpad=15, va='center', ha="right")
+axs[1][0].set_ylabel("Sérsic Index\ndCor = 0.74", fontsize=60, rotation=0, labelpad=15, va="center", ha="right")
+axs[2][0].set_ylabel("Half-Light Radius\ndCor = 0.81", fontsize=60, rotation=0, labelpad=15, va="center", ha="right")
 
 
 fig.subplots_adjust(wspace=0, hspace=0.05)
