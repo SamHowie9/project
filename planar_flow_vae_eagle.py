ccdfa14f28e0c0519f142af82453e78a8b720378
Sam <samhowie9@gmail.com>
2025-07-02 16:38:20 +0100

    Code

--- planar_flow_vae_eagle.py
+++ planar_flow_vae_eagle.py
@@ -24,8 +24,8 @@ tfd = tfp.distributions
 
 
 
-run = 2
-encoding_dim = 41
+run = 3
+encoding_dim = 25
 n_flows = 0
 beta = 0.0001
 beta_name = "0001"
@@ -35,7 +35,7 @@ batch_size = 32
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="5"
+os.environ["CUDA_VISIBLE_DEVICES"]="6"
 
 
 
