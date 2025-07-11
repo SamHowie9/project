71c13d2421d703446c927057dce6e24c1a5b0919
Sam <samhowie9@gmail.com>
2025-07-11 11:57:09 +0100

    Code

--- planar_flow_vae_eagle.py
+++ planar_flow_vae_eagle.py
@@ -25,7 +25,7 @@ tfd = tfp.distributions
 
 
 run = 3
-encoding_dim = 40
+encoding_dim = 50
 n_flows = 0
 beta = 0.0001
 beta_name = "0001"
@@ -51,7 +51,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0"
 # for encoding_dim in [49, 50]:
 # for encoding_dim in [encoding_dim]:
 # for run in [1, 2, 3]:
-for run in range(11, 26):
+for run in range(11, 16):
 # for run in [run]:
 # for run in [run, run+1, run+2]:
 # for encoding_dim in range(encoding_dim, encoding_dim+3):
