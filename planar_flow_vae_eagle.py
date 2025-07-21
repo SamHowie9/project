9696e769c717095ee2a7188cb2ef388851493a2b
Sam <samhowie9@gmail.com>
2025-07-21 12:48:51 +0100

    Code

--- planar_flow_vae_eagle.py
+++ planar_flow_vae_eagle.py
@@ -291,10 +291,10 @@ print()
 # for run, encoding_dim in [[3, 29], [3, 31], [3, 32], [2, 35], [3, 35], [2, 36], [2, 37], [2, 38], [3, 38], [2, 39], [2, 40], [3, 43], [2, 44], [2, 45], [2, 46], [2, 47], [2, 48], [2, 49], [2, 50]]:
 # for run, encoding_dim in [[4, 40], [5, 40], [6, 40], [7, 40], [8, 40], [9, 40], [10, 40], [4, 50], [5, 50], [6, 50], [7, 50], [8, 50], [9, 50], [10, 50]]:
 # for run in [1, 2, 3]:
-for run in range(17, 26):
+# for run in range(17, 26):
 # for encoding_dim in range(10, 26):
 # for encoding_dim in [20, 21, 22, 23, 24, 25]:
-
+for run in [run]:
 
 
 
@@ -531,12 +531,12 @@ for run in range(17, 26):
 
 
     # train the model
-    # model_loss = vae.fit(train_images, epochs=epochs, batch_size=batch_size)
+    model_loss = vae.fit(train_images, epochs=epochs, batch_size=batch_size)
 
     vae.build(input_shape=(None, 256, 256, 3))
 
     # or load the weights from a previous run
-    vae.load_weights("Variational Eagle/Weights/Normalising Flow Balanced/planar_new_latent_" + str(encoding_dim) + "_beta_" + beta_name + "_epoch_" + str(epochs) + "_flows_" + str(n_flows) + "_" + str(run) + "_default.weights.h5")
+    # vae.load_weights("Variational Eagle/Weights/Normalising Flow Balanced/planar_new_latent_" + str(encoding_dim) + "_beta_" + beta_name + "_epoch_" + str(epochs) + "_flows_" + str(n_flows) + "_" + str(run) + "_default.weights.h5")
 
     # save the weights
     vae.save_weights(filepath="Variational Eagle/Weights/Normalising Flow Balanced/planar_new_latent_" + str(encoding_dim) + "_beta_" + beta_name + "_epoch_" + str(epochs) + "_flows_" + str(n_flows) + "_" + str(run) + "_default.weights.h5", overwrite=True)
