b1aac812d1f21c9feba78a4efa6f17ba853e7e70
Sam <samhowie9@gmail.com>
2025-08-13 15:27:13 +0100

    Code

--- planar_flow_vae_eagle.py
+++ planar_flow_vae_eagle.py
@@ -35,7 +35,7 @@ batch_size = 32
 
 # select which gpu to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="5"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 
 
@@ -279,7 +279,7 @@ print()
 
 
 for encoding_dim in [encoding_dim]:
-    for run in [13, 14, 15]:
+    for run in [16, 17, 18, 19, 20, 21, 22, 23, 24, 25]:
 
 
 
