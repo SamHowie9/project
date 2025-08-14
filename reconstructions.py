b989ef8665f85d4dc600fc499d18ea81b354f398
Sam <samhowie9@gmail.com>
2025-08-14 17:28:25 +0100

    Code

--- reconstructions.py
+++ reconstructions.py
@@ -523,7 +523,7 @@ for run in [2, 5, 7, 10, 12, 15, 17, 18, 19, 20, 22, 23]:
 
     # pca reconstructions by feature
 
-    fig, axs = plt.subplots(11, len(reconstruction_indices), figsize=(len(reconstruction_indices)*5, 11*5))
+    fig, axs = plt.subplots(12, len(reconstruction_indices), figsize=(len(reconstruction_indices)*5, 12*5))
 
     for i in range(0, len(reconstruction_indices)):
 
@@ -535,7 +535,7 @@ for run in [2, 5, 7, 10, 12, 15, 17, 18, 19, 20, 22, 23]:
         dt = all_properties.loc[reconstruction_indices[i], "DiscToTotal"]
         axs[0][i].set_title("D/T=" + str(round(dt, 3)), fontsize=45, pad=10)
 
-        for j, feat in enumerate(range(10, 0, -1)):
+        for j, feat in enumerate(range(11, 0, -1)):
 
             pca = PCA(n_components=feat, svd_solver="full").fit(extracted_features)
 
