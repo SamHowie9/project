ff1ff6d1aca1a8804e6401af4e1101f7a6410261
Sam <samhowie9@gmail.com>
2025-08-19 12:36:54 +0100

    Code

--- correlation_flows.py
+++ correlation_flows.py
@@ -262,8 +262,9 @@ correlation_df = pd.DataFrame({col: sorted(correlation_df[col], reverse=True) fo
 
 # order the annotation dataframe
 for i, col in enumerate(correlation_text_df.columns):
-    print(correlation_text_df[col].apply(lambda x: float(x.split(': ')[1])).sort_values(ascending=False).index)
     print(col)
+    print(correlation_text_df[col])
+    print(correlation_text_df[col].apply(lambda x: float(x.split(': ')[1])).sort_values(ascending=False).index)
     correlation_text_df[col] = correlation_text_df[col].iloc[
         correlation_text_df[col].apply(lambda x: float(x.split(': ')[1])).sort_values(ascending=False).index
     ].values
