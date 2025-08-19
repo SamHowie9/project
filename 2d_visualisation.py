0a2708c27d00867e9715fd07b68b870ce0d5483d
Sam <samhowie9@gmail.com>
2025-08-19 14:53:50 +0100

    Code

--- 2d_visualisation.py
+++ 2d_visualisation.py
@@ -24,66 +24,17 @@ pd.set_option('display.width', None)
 
 
 
-# load structural and physical properties into dataframes
-# structure_properties = pd.read_csv("Galaxy Properties/Eagle Properties/structure_propeties.csv", comment="#")
-# physical_properties = pd.read_csv("Galaxy Properties/Eagle Properties/physical_properties.csv", comment="#")
-#
-# # dataframe for all properties
-# all_properties = pd.merge(structure_properties, physical_properties, on="GalaxyID")
-#
-# # load the non parametric properties (restructure the dataframe to match the others)
-# non_parametric_properties = pd.read_hdf("Galaxy Properties/Eagle Properties/Ref100N1504.hdf5", key="galface/r")
-# non_parametric_properties = non_parametric_properties.reset_index()
-# non_parametric_properties = non_parametric_properties.sort_values(by="GalaxyID")
-#
-# # load the disk-total ratios
-# disk_total = pd.read_csv("Galaxy Properties/Eagle Properties/disk_to_total.csv", comment="#")
-#
-#
-# # add the non parametric properties, and the disk-total to the other properties dataframe
-# all_properties = pd.merge(all_properties, non_parametric_properties, on="GalaxyID")
-# all_properties = pd.merge(all_properties, disk_total, on="GalaxyID")
-
-# find all bad fit galaxies
-# bad_fit = all_properties[((all_properties["flag_r"] == 1) |
-#                           (all_properties["flag_r"] == 4) |
-#                           (all_properties["flag_r"] == 5) |
-#                           (all_properties["flag_r"] == 6))].index.tolist()
-#
-# # remove those galaxies
-# for galaxy in bad_fit:
-#     all_properties = all_properties.drop(galaxy, axis=0)
-#
-# # reset the index values
-# all_properties = all_properties.reset_index(drop=True)
-#
-# # account for the training data
-# all_properties = all_properties.iloc[:-200]
-
-
 
 all_properties = pd.read_csv("Galaxy Properties/Eagle Properties/all_properties_balanced.csv")
 
 
 
 
-
-# extracted_features_all = np.load("Variational Eagle/Extracted Features/Normalising Flow/planar_new_latent_" + str(encoding_dim) + "_beta_" + beta_name + "_epoch_" + str(epochs) + "_flows_" + str(n_flows) + "_" + str(run) + "_default_transformed.npy")
 extracted_features = np.load("Variational Eagle/Extracted Features/Normalising Flow Balanced/planar_new_latent_" + str(encoding_dim) + "_beta_" + beta_name + "_epoch_" + str(epochs) + "_flows_" + str(n_flows) + "_" + str(run) + "_default_transformed.npy")
-
-# extracted_features_real = extracted_features_all[:len(all_properties)]
-
-
-
-
-print(extracted_features.shape)
 print(extracted_features.shape)
 
 pca = PCA(n_components=0.999, svd_solver="full").fit(extracted_features)
 extracted_features = pca.transform(extracted_features)
-
-
-print(extracted_features.shape)
 print(extracted_features.shape)
 
 
@@ -98,107 +49,21 @@ print(extracted_features.shape)
 #     else:
 #         return "Transitional"
 #
-# # def classify_morphology(n):
-# #
-# #     if n <= 2.5:
-# #         return "Spiral"
-# #     elif n >= 4:
-# #         return "Elliptical"
-# #     else:
-# #         return "Transitional"
-#
 # morphology = all_properties["DiscToTotal"].apply(classify_morphology).tolist()
-# # morphology = all_properties["n_r"].apply(classify_morphology).tolist()
-
 # print(morphology)
 
 
-# for i in range(extracted_features_all.shape[0] - len(morphology)):
-#     morphology.append("Augmented")
 
 
-# for i in range(2394):
-#     morphology.append("Elliptical")
-# for i in range(2552):
-#     morphology.append("Transitional")
-
-
-# print(len(morphology))
-
-
-
-spirals_indices = all_properties[all_properties["DiscToTotal"] > 0.2].index.tolist()
-transitional_indices = all_properties[all_properties["DiscToTotal"].between(0.1, 0.2, inclusive="both")].index.tolist()
-elliptical_indices = all_properties[all_properties["DiscToTotal"] < 0.1].index.tolist()
-print(spirals_indices)
-print(len(spirals_indices))
-
-
-
-
-
-# # tsne = TSNE(n_components=2, random_state=0).fit_transform(extracted_features)
-#
-# # umap = UMAP().fit_transform(extracted_features)
-#
-# print(tsne.shape)
-#
-# tsne = tsne[transitional_indices]
-#
-# print(tsne.shape)
+# spirals_indices = all_properties[all_properties["DiscToTotal"] > 0.2].index.tolist()
+# transitional_indices = all_properties[all_properties["DiscToTotal"].between(0.1, 0.2, inclusive="both")].index.tolist()
+# elliptical_indices = all_properties[all_properties["DiscToTotal"] < 0.1].index.tolist()
 
 
 
 
 
 
-# fig, axs = plt.subplots(1, 1, figsize=(10, 10))
-#
-#
-# palette = ["C0", "C1", "#D3D3D3"]
-#
-# size_map = {"Spiral": 20, "Elliptical": 20, "Transitional": 10}
-# sizes = [size_map[m] for m in morphology]
-#
-# alpha_map = {"Spiral": 1.0, "Transitional": 0.3, "Elliptical": 1.0}
-# alpha = [alpha_map[m] for m in morphology]
-#
-# # sns.scatterplot(x=tsne.T[0], y=tsne.T[1], ax=axs, linewidth=0, s=20)
-# # sns.scatterplot(x=tsne.T[0], y=tsne.T[1], ax=axs, hue=morphology, palette="colorblind", linewidth=0, s=20)
-# # sns.scatterplot(x=tsne.T[0], y=tsne.T[1], ax=axs, hue=morphology, palette=palette, linewidth=0, s=20)
-# sns.scatterplot(x=tsne.T[0], y=tsne.T[1], ax=axs, hue=morphology, palette=palette, linewidth=0, size=sizes, sizes=(10, 20), legend="brief")
-#
-# handles, labels = axs.get_legend_handles_labels()
-# filtered = [(h, l) for h, l in zip(handles, labels) if not l.isdigit()]
-# handles_filtered, labels_filtered = zip(*filtered)
-# axs.legend(handles_filtered, labels_filtered)
-#
-# # axs.set_xlim(-95, 95)
-# # axs.set_ylim(-95, 95)
-#
-# plt.savefig("Variational Eagle/Plots/tsne_nop_flow_pca_morphology_" + str(n_flows) + "_" + str(run), bbox_inches="tight")
-# plt.show()
-
-
-
-
-
-
-# fig, axs = plt.subplots(1, 1, figsize=(12, 10))
-#
-#
-# # norm = TwoSlopeNorm(vmin=all_properties["DiscToTotal"].min(), vcenter=0.15, vmax=all_properties["DiscToTotal"].max())
-# norm = TwoSlopeNorm(vmin=0, vcenter=0.15, vmax=1)
-#
-# # sns.scatterplot(x=tsne.T[0], y=tsne.T[1], ax=axs, c=all_properties["DiscToTotal"], cmap="jet", linewidth=0, size=20)
-# # tsne_scatter = axs.scatter(x=tsne.T[0], y=tsne.T[1], c=all_properties["DiscToTotal"], cmap="Blues", s=10)
-# # tsne_scatter = axs.scatter(x=tsne.T[0], y=tsne.T[1], c=all_properties["DiscToTotal"], cmap="RdYlBu", norm=norm, s=10)
-# tsne_scatter = axs.scatter(x=tsne.T[0], y=tsne.T[1], c=all_properties["DiscToTotal"].loc[all_properties["DiscToTotal"].between(0.1, 0.2, inclusive="both")], cmap="RdYlBu", norm=norm, s=10)
-#
-# # cbar = plt.colorbar(tsne_scatter, ax=axs, label="Disk-Total Ratio")
-# # cbar.ax.yaxis.set_label_position('left')
-
-print(all_properties)
 
 
 fig, axs=plt.subplots(1, 1, figsize=(10, 10))
@@ -208,10 +73,16 @@ plt.show()
 
 
 
-# umap = UMAP(n_components=2, init="pca", random_state=0).fit_transform(extracted_features)
+
+
+
+
+
+
+umap = UMAP(n_components=1, init="spectral", random_state=0).fit_transform(extracted_features)
 # np.save("Variational Eagle/2D Visualisation/umap_pca.npy", umap)
 
-umap = np.load("Variational Eagle/2D Visualisation/umap_spectral.npy")
+# umap = np.load("Variational Eagle/2D Visualisation/umap_spectral.npy")
 
 
 # norm = TwoSlopeNorm(vmin=all_properties["n_r"].min(), vcenter=1.5, vmax=all_properties["n_r"].max())
@@ -225,13 +96,16 @@ fig, axs = plt.subplots(1, 1, figsize=(14, 10))
 # scatter = axs.scatter(x=umap.T[0], y=umap.T[1], s=10)
 # scatter = axs.scatter(x=umap.T[0], y=umap.T[1], c=all_properties["MassType_Star"], cmap="RdYlBu", norm=norm, s=10)
 # scatter = axs.scatter(x=umap.T[0], y=umap.T[1], c=all_properties["rhalf_ellip"], cmap="RdYlBu", vmin=all_properties["re_r"].min(), vmax=15, s=10)
-scatter = axs.scatter(x=umap.T[0], y=umap.T[1], c=all_properties["rhalf_ellip"], cmap="RdYlBu", s=10)
+# scatter = axs.scatter(x=umap.T[0], y=umap.T[1], c=all_properties["rhalf_ellip"], cmap="RdYlBu", s=10)
 
-cbar = plt.colorbar(scatter, ax=axs, label="Half-Light Radius")
-cbar.ax.yaxis.set_label_position('left')
+# cbar = plt.colorbar(scatter, ax=axs, label="Half-Light Radius")
+# cbar.ax.yaxis.set_label_position('left')
+
+
+scatter = axs.scatter(x=umap, y=all_properties["n_r"])
 
 
 
-plt.savefig("Variational Eagle/2D Visualisation/umap_half_light_radius_" + str(encoding_dim) + "_" + str(run), bbox_inches="tight")
+# plt.savefig("Variational Eagle/2D Visualisation/umap_half_light_radius_" + str(encoding_dim) + "_" + str(run), bbox_inches="tight")
 # plt.savefig("Variational Eagle/2D Visualisation/pca", bbox_inches="tight")
 plt.show()
